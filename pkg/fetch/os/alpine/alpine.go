461a66a3791ab12a9ec1c1b4d0b107609e9ba2a9
MaineK00n <mainek00n.1229@gmail.com>
2022-07-11 11:59:23 +0900

    feat(fetch/alpine): add alpine linux

---
+++ pkg/fetch/os/alpine/alpine.go
@@ -0,0 +1,205 @@
+package alpine
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/pkg/errors"
+
+	"github.com/MaineK00n/vuls-data-update/pkg/fetch/util"
+)
+
+const baseURL = "https://secdb.alpinelinux.org/"
+
+type options struct {
+	baseURL string
+	dir     string
+	retry   int
+}
+
+type Option interface {
+	apply(*options)
+}
+
+type baseURLOption string
+
+func (u baseURLOption) apply(opts *options) {
+	opts.baseURL = string(u)
+}
+
+func WithBaseURL(url string) Option {
+	return baseURLOption(url)
+}
+
+type dirOption string
+
+func (d dirOption) apply(opts *options) {
+	opts.dir = string(d)
+}
+
+func WithDir(dir string) Option {
+	return dirOption(dir)
+}
+
+type retryOption int
+
+func (r retryOption) apply(opts *options) {
+	opts.retry = int(r)
+}
+
+func WithRetry(retry int) Option {
+	return retryOption(retry)
+}
+
+func Fetch(opts ...Option) error {
+	options := &options{
+		baseURL: baseURL,
+		dir:     filepath.Join(util.CacheDir(), "source", "alpine"),
+		retry:   3,
+	}
+
+	for _, o := range opts {
+		o.apply(options)
+	}
+
+	releases, err := options.walkIndexOf()
+	if err != nil {
+		return errors.Wrap(err, "walk index of")
+	}
+
+	for _, r := range releases {
+		log.Printf("[INFO] Fetch Alpine Linux %s", r)
+		files, err := options.walkDistroVersion(r)
+		if err != nil {
+			return errors.Wrapf(err, "walk alpine linux %s", r)
+		}
+
+		advs, err := options.fetchAdvisory(r, files)
+		if err != nil {
+			return errors.Wrapf(err, "fetch alpine linux %s advisory %q", r, files)
+		}
+
+		dir := filepath.Join(options.dir, r)
+		if err := os.RemoveAll(dir); err != nil {
+			return errors.Wrapf(err, "remove %s", dir)
+		}
+		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+			return errors.Wrapf(err, "mkdir %s", dir)
+		}
+		for filename, adv := range advs {
+			if err := func() error {
+				f, err := os.Create(filepath.Join(dir, filename))
+				if err != nil {
+					return errors.Wrapf(err, "create %s", filepath.Join(dir, filename))
+				}
+				defer f.Close()
+
+				enc := json.NewEncoder(f)
+				enc.SetIndent("", "  ")
+				if err := enc.Encode(adv); err != nil {
+					return errors.Wrap(err, "encode data")
+				}
+				return nil
+			}(); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (opts options) walkIndexOf() ([]string, error) {
+	bs, err := util.FetchURL(opts.baseURL, opts.retry)
+	if err != nil {
+		return nil, errors.Wrap(err, "fetch index of")
+	}
+
+	d, err := goquery.NewDocumentFromReader(bytes.NewReader(bs))
+	if err != nil {
+		return nil, errors.Wrap(err, "parse as html")
+	}
+
+	var releases []string
+	d.Find("a").Each(func(i int, selection *goquery.Selection) {
+		txt := selection.Text()
+		if !strings.HasPrefix(txt, "v") {
+			return
+		}
+		releases = append(releases, strings.TrimSuffix(txt, "/"))
+	})
+	return releases, nil
+}
+
+func (opts options) walkDistroVersion(release string) ([]string, error) {
+	u, err := url.JoinPath(opts.baseURL, release)
+	if err != nil {
+		return nil, errors.Wrap(err, "join url path")
+	}
+
+	bs, err := util.FetchURL(u, opts.retry)
+	if err != nil {
+		return nil, errors.Wrapf(err, "fetch alpine linux %s index of", release)
+	}
+
+	d, err := goquery.NewDocumentFromReader(bytes.NewReader(bs))
+	if err != nil {
+		return nil, errors.Wrap(err, "parse as html")
+	}
+
+	var files []string
+	d.Find("a").Each(func(i int, selection *goquery.Selection) {
+		txt := selection.Text()
+		if !strings.HasSuffix(txt, ".json") {
+			return
+		}
+		files = append(files, txt)
+	})
+	return files, nil
+}
+
+func (opts options) fetchAdvisory(release string, files []string) (map[string]Advisory, error) {
+	advs := make(map[string]Advisory, 2)
+	for _, f := range files {
+		u, err := url.JoinPath(opts.baseURL, release, f)
+		if err != nil {
+			return nil, errors.Wrap(err, "join url path")
+		}
+
+		bs, err := util.FetchURL(u, opts.retry)
+		if err != nil {
+			return nil, errors.Wrapf(err, "fetch alpine linux %s %s", release, f)
+		}
+
+		var secdb secdb
+		if err := json.Unmarshal(bs, &secdb); err != nil {
+			return nil, errors.Wrapf(err, "unmarshal alpine linux %s %s", release, f)
+		}
+
+		advs[f] = Advisory{
+			Apkurl:        secdb.Apkurl,
+			Archs:         secdb.Archs,
+			Reponame:      secdb.Reponame,
+			Urlprefix:     secdb.Urlprefix,
+			Distroversion: secdb.Distroversion,
+			Packages:      map[string][]Package{},
+		}
+
+		for _, p := range secdb.Packages {
+			for v, cves := range p.Pkg.Secfixes {
+				for _, cve := range cves {
+					advs[f].Packages[cve] = append(advs[f].Packages[cve], Package{
+						Name:    p.Pkg.Name,
+						Version: v,
+					})
+				}
+			}
+		}
+	}
+	return advs, nil
+}
---
+++ pkg/fetch/os/alpine/types.go
@@ -0,0 +1,30 @@
+package alpine
+
+// secdb represents a type included in files from the Alpine repository
+type secdb struct {
+	Apkurl        string   `json:"apkurl"`
+	Archs         []string `json:"archs"`
+	Reponame      string   `json:"reponame"`
+	Urlprefix     string   `json:"urlprefix"`
+	Distroversion string   `json:"distroversion"`
+	Packages      []struct {
+		Pkg struct {
+			Name     string              `json:"name"`
+			Secfixes map[string][]string `json:"secfixes"`
+		} `json:"pkg"`
+	} `json:"packages"`
+}
+
+type Advisory struct {
+	Apkurl        string               `json:"apkurl"`
+	Archs         []string             `json:"archs"`
+	Reponame      string               `json:"reponame"`
+	Urlprefix     string               `json:"urlprefix"`
+	Distroversion string               `json:"distroversion"`
+	Packages      map[string][]Package `json:"packages"`
+}
+
+type Package struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
