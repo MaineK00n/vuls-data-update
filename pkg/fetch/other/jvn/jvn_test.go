d616108b8534f0ff166b7ee9b5b075b373199845
MaineK00n <mainek00n.1229@gmail.com>
2022-08-30 12:35:58 +0900

    feat(fetch/jvn): add jvn

---
+++ pkg/fetch/other/jvn/jvn_test.go
@@ -0,0 +1,89 @@
+package jvn_test
+
+import (
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/MaineK00n/vuls-data-update/pkg/fetch/other/jvn"
+)
+
+func TestFetch(t *testing.T) {
+	tests := []struct {
+		name     string
+		files    []string
+		hasError bool
+	}{
+		{
+			name:  "happy path",
+			files: []string{"testdata/fixtures/jvndb_detail_2022.rdf"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				testdata := strings.TrimPrefix(r.URL.Path, string(os.PathSeparator))
+				if _, err := os.Stat(testdata); err != nil {
+					http.NotFound(w, r)
+					return
+				}
+				http.ServeFile(w, r, testdata)
+			}))
+			defer ts.Close()
+
+			urls := make([]string, 0, len(tt.files))
+			for _, f := range tt.files {
+				u, err := url.JoinPath(ts.URL, f)
+				if err != nil {
+					t.Error("unexpected error:", err)
+				}
+				urls = append(urls, u)
+			}
+			dir := t.TempDir()
+			err := jvn.Fetch(jvn.WithURLs(urls), jvn.WithDir(dir), jvn.WithRetry(0))
+			switch {
+			case err != nil && !tt.hasError:
+				t.Error("unexpected error:", err)
+			case err == nil && tt.hasError:
+				t.Error("expected error has not occurred")
+			}
+
+			if err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+
+				if info.IsDir() {
+					return nil
+				}
+
+				dir, file := filepath.Split(path)
+				want, err := os.ReadFile(filepath.Join("testdata", "golden", filepath.Base(dir), file))
+				if err != nil {
+					return err
+				}
+
+				got, err := os.ReadFile(path)
+				if err != nil {
+					return err
+				}
+
+				if diff := cmp.Diff(want, got); diff != "" {
+					t.Errorf("Fetch(). (-expected +got):\n%s", diff)
+				}
+
+				return nil
+			}); err != nil {
+				t.Error("walk error:", err)
+			}
+		})
+	}
+}
