7282c281f70603431bc11ce6fb2976de77a29809
MaineK00n <mainek00n.1229@gmail.com>
2024-03-22 02:09:51 +0900

    feat(fetch/alma/oval): add alma oval

---
+++ pkg/fetch/alma/oval/oval.go
@@ -0,0 +1,85 @@
+package oval
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+
+	"github.com/MaineK00n/vuls-data-update/pkg/fetch/util"
+	utilhttp "github.com/MaineK00n/vuls-data-update/pkg/fetch/util/http"
+)
+
+const baseURL = "https://security.almalinux.org/oval/"
+
+type options struct {
+	baseURL string
+	dir     string
+	retry   int
+}
+
+type Option interface {
+	apply(*options)
+}
+
+type baseURLOption string
+
+func (u baseURLOption) apply(opts *options) {
+	opts.baseURL = string(u)
+}
+
+func WithBaseURL(url string) Option {
+	return baseURLOption(url)
+}
+
+type dirOption string
+
+func (d dirOption) apply(opts *options) {
+	opts.dir = string(d)
+}
+
+func WithDir(dir string) Option {
+	return dirOption(dir)
+}
+
+type retryOption int
+
+func (r retryOption) apply(opts *options) {
+	opts.retry = int(r)
+}
+
+func WithRetry(retry int) Option {
+	return retryOption(retry)
+}
+
+func Fetch(opts ...Option) error {
+	options := &options{
+		baseURL: baseURL,
+		dir:     filepath.Join(util.CacheDir(), "fetch", "alma", "oval"),
+		retry:   3,
+	}
+
+	for _, o := range opts {
+		o.apply(options)
+	}
+
+	if err := util.RemoveAll(options.dir); err != nil {
+		return errors.Wrapf(err, "remove %s", options.dir)
+	}
+
+	log.Println("[INFO] Fetch AlmaLinux OVAL")
+	resp, err := utilhttp.NewClient(utilhttp.WithClientRetryMax(options.retry)).Get(options.baseURL)
+	if err != nil {
+		return errors.Wrap(err, "fetch repository")
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		_, _ = io.Copy(io.Discard, resp.Body)
+		return errors.Errorf("error request response with status code %d", resp.StatusCode)
+	}
+
+	return nil
+}
