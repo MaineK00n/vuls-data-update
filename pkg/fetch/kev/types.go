68486bad3edc49093502a7d8e6df40104d5cdd13
MaineK00n <mainek00n.1229@gmail.com>
2024-07-24 19:19:48 +0900

    feat(extract/kev): add kev (#209)

--- pkg/fetch/kev/types.go
+++ pkg/fetch/kev/types.go
@@ -11,14 +11,15 @@ type catalog struct {
 }
 
 type Vulnerability struct {
-	CveID                      string `json:"cveID,omitempty"`
-	VendorProject              string `json:"vendorProject,omitempty"`
-	Product                    string `json:"product,omitempty"`
-	VulnerabilityName          string `json:"vulnerabilityName,omitempty"`
-	ShortDescription           string `json:"shortDescription,omitempty"`
-	RequiredAction             string `json:"requiredAction,omitempty"`
-	KnownRansomwareCampaignUse string `json:"knownRansomwareCampaignUse,omitempty"`
-	Notes                      string `json:"notes,omitempty"`
-	DateAdded                  string `json:"dateAdded,omitempty"`
-	DueDate                    string `json:"dueDate,omitempty"`
+	CveID                      string   `json:"cveID,omitempty"`
+	VendorProject              string   `json:"vendorProject,omitempty"`
+	Product                    string   `json:"product,omitempty"`
+	VulnerabilityName          string   `json:"vulnerabilityName,omitempty"`
+	ShortDescription           string   `json:"shortDescription,omitempty"`
+	RequiredAction             string   `json:"requiredAction,omitempty"`
+	KnownRansomwareCampaignUse string   `json:"knownRansomwareCampaignUse,omitempty"`
+	Notes                      string   `json:"notes,omitempty"`
+	DateAdded                  string   `json:"dateAdded,omitempty"`
+	DueDate                    string   `json:"dueDate,omitempty"`
+	CWEs                       []string `json:"cwes,omitempty"`
 }
