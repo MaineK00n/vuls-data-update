9cc9ca312fe797bd5e5e4ba568a1825b2d1ff6c7
MaineK00n <mainek00n.1229@gmail.com>
2024-09-07 03:33:55 +0900

    feat(extract/types/.../ecosystem): add GetEcosystem (#238)

--- pkg/extract/types/data/detection/ecosystem/ecosystem.go
+++ pkg/extract/types/data/detection/ecosystem/ecosystem.go
@@ -1,5 +1,12 @@
 package ecosystem
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
 type Ecosystem string
 
 const (
@@ -42,3 +49,110 @@ const (
 	EcosystemTypeRubygems = "rubygems"
 	EcosystemTypeSwift    = "swift"
 )
+
+func GetEcosystem(family, release string) (Ecosystem, error) {
+	switch family {
+	case EcosystemTypeAlma:
+		return Ecosystem(fmt.Sprintf("%s:%s", family, strings.Split(release, ".")[0])), nil
+	case EcosystemTypeAlpine:
+		ss := strings.Split(release, ".")
+		if len(ss) < 2 {
+			return "", errors.Errorf("unexpected release format. expected: %q, actual: %q", "<major>.<minor>(.<patch>)", release)
+		}
+		return Ecosystem(fmt.Sprintf("%s:%s.%s", family, ss[0], ss[1])), nil
+	case EcosystemTypeAmazon:
+		return Ecosystem(fmt.Sprintf("%s:%s", family, strings.Split(release, ".")[0])), nil
+	case EcosystemTypeArch:
+		return Ecosystem(family), nil
+	case EcosystemTypeDebian:
+		return Ecosystem(fmt.Sprintf("%s:%s", family, strings.Split(release, ".")[0])), nil
+	case EcosystemTypeEPEL:
+		return Ecosystem(fmt.Sprintf("%s:%s", family, release)), nil
+	case EcosystemTypeFedora:
+		return Ecosystem(fmt.Sprintf("%s:%s", family, release)), nil
+	case EcosystemTypeFreeBSD:
+		return Ecosystem(family), nil
+	case EcosystemTypeGentoo:
+		return Ecosystem(family), nil
+	case EcosystemTypeNetBSD:
+		return Ecosystem(family), nil
+	case EcosystemTypeOracle:
+		return Ecosystem(fmt.Sprintf("%s:%s", family, strings.Split(release, ".")[0])), nil
+	case EcosystemTypeRedHat:
+		return Ecosystem(fmt.Sprintf("%s:%s", family, strings.Split(release, ".")[0])), nil
+	case EcosystemTypeRocky:
+		return Ecosystem(fmt.Sprintf("%s:%s", family, strings.Split(release, ".")[0])), nil
+	case EcosystemTypeOpenSUSE:
+		if release == "tumbleweed" {
+			return Ecosystem(fmt.Sprintf("%s:%s", family, release)), nil
+		}
+		if strings.HasPrefix(release, "leap:") {
+			ss := strings.Split(strings.TrimPrefix(release, "leap:"), ".")
+			if len(ss) < 2 {
+				return "", errors.Errorf("unexpected release format. expected: %q, actual: %q", "leap:<major>.<minor>(.<patch>)", release)
+			}
+			return Ecosystem(fmt.Sprintf("%s-leap:%s.%s", family, ss[0], ss[1])), nil
+		}
+		ss := strings.Split(release, ".")
+		if len(ss) < 2 {
+			return "", errors.Errorf("unexpected release format. expected: %q, actual: %q", "<major>.<minor>(.<patch>)", release)
+		}
+		return Ecosystem(fmt.Sprintf("%s:%s.%s", family, ss[0], ss[1])), nil
+	case EcosystemTypeSUSEServer:
+		ss := strings.Split(release, ".")
+		if len(ss) < 2 {
+			return "", errors.Errorf("unexpected release format. expected: %q, actual: %q", "<major>.<minor>(.<patch>)", release)
+		}
+		return Ecosystem(fmt.Sprintf("%s:%s.%s", family, ss[0], ss[1])), nil
+	case EcosystemTypeSUSEDesktop:
+		ss := strings.Split(release, ".")
+		if len(ss) < 2 {
+			return "", errors.Errorf("unexpected release format. expected: %q, actual: %q", "<major>.<minor>(.<patch>)", release)
+		}
+		return Ecosystem(fmt.Sprintf("%s:%s.%s", family, ss[0], ss[1])), nil
+	case EcosystemTypeUbuntu:
+		ss := strings.Split(release, ".")
+		if len(ss) < 2 {
+			return "", errors.Errorf("unexpected release format. expected: %q, actual: %q", "<major>.<minor>(.<patch>)", release)
+		}
+		return Ecosystem(fmt.Sprintf("%s:%s.%s", family, ss[0], ss[1])), nil
+	case EcosystemTypeWindows:
+		return Ecosystem(fmt.Sprintf("%s:%s", family, release)), nil
+	case EcosystemTypeCPE:
+		return Ecosystem(family), nil
+	case EcosystemTypeFortinet:
+		return Ecosystem(family), nil
+	case EcosystemTypeCargo:
+		return Ecosystem(family), nil
+	case EcosystemTypeComposer:
+		return Ecosystem(family), nil
+	case EcosystemTypeConan:
+		return Ecosystem(family), nil
+	case EcosystemTypeErlang:
+		return Ecosystem(family), nil
+	case EcosystemTypeGolang:
+		return Ecosystem(family), nil
+	case EcosystemTypeHaskell:
+		return Ecosystem(family), nil
+	case EcosystemTypeMaven:
+		return Ecosystem(family), nil
+	case EcosystemTypeNpm:
+		return Ecosystem(family), nil
+	case EcosystemTypeNuget:
+		return Ecosystem(family), nil
+	case EcosystemTypePerl:
+		return Ecosystem(family), nil
+	case EcosystemTypePip:
+		return Ecosystem(family), nil
+	case EcosystemTypePub:
+		return Ecosystem(family), nil
+	case EcosystemTypeR:
+		return Ecosystem(family), nil
+	case EcosystemTypeRubygems:
+		return Ecosystem(family), nil
+	case EcosystemTypeSwift:
+		return Ecosystem(family), nil
+	default:
+		return "", errors.Errorf("unexpected family. expected: %q, actual: %q", []Ecosystem{EcosystemTypeAlma, EcosystemTypeAlpine, EcosystemTypeAmazon, EcosystemTypeArch, EcosystemTypeDebian, EcosystemTypeEPEL, EcosystemTypeFedora, EcosystemTypeFreeBSD, EcosystemTypeGentoo, EcosystemTypeNetBSD, EcosystemTypeOracle, EcosystemTypeRedHat, EcosystemTypeRocky, EcosystemTypeOpenSUSE, EcosystemTypeSUSEServer, EcosystemTypeSUSEDesktop, EcosystemTypeUbuntu, EcosystemTypeWindows, EcosystemTypeCPE, EcosystemTypeFortinet, EcosystemTypeCargo, EcosystemTypeComposer, EcosystemTypeConan, EcosystemTypeErlang, EcosystemTypeGolang, EcosystemTypeHaskell, EcosystemTypeMaven, EcosystemTypeNpm, EcosystemTypeNuget, EcosystemTypePerl, EcosystemTypePip, EcosystemTypePub, EcosystemTypeR, EcosystemTypeRubygems, EcosystemTypeSwift}, family)
+	}
+}
---
+++ pkg/extract/types/data/detection/ecosystem/ecosystem_test.go
@@ -0,0 +1,57 @@
+package ecosystem_test
+
+import (
+	"testing"
+
+	"github.com/MaineK00n/vuls-data-update/pkg/extract/types/data/detection/ecosystem"
+)
+
+func TestGetEcosystem(t *testing.T) {
+	type args struct {
+		family  string
+		release string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    ecosystem.Ecosystem
+		wantErr bool
+	}{
+		{
+			name: "oracle 9",
+			args: args{
+				family:  "oracle",
+				release: "9",
+			},
+			want: ecosystem.Ecosystem("oracle:9"),
+		},
+		{
+			name: "oracle 9.0",
+			args: args{
+				family:  "oracle",
+				release: "9.0",
+			},
+			want: ecosystem.Ecosystem("oracle:9"),
+		},
+		{
+			name: "cpe",
+			args: args{
+				family:  "cpe",
+				release: "",
+			},
+			want: ecosystem.Ecosystem("cpe"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ecosystem.GetEcosystem(tt.args.family, tt.args.release)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetEcosystem() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetEcosystem() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
