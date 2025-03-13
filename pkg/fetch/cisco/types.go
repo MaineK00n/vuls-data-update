20fc1b35a5b47163c6c74459442b79048a3a0887
MaineK00n <mainek00n.1229@gmail.com>
2025-03-14 05:13:00 +0900

    feat(fetch/cisco): add cisco

---
+++ pkg/fetch/cisco/types.go
@@ -0,0 +1,32 @@
+package cisco
+
+type accessTokenResponse struct {
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int    `json:"expires_in"`
+	AccessToken string `json:"access_token"`
+	Scope       string `json:"scope"`
+}
+
+type advisories struct {
+	Advisories []Advisory `json:"advisories"`
+}
+
+type Advisory struct {
+	AdvisoryID     string      `json:"advisoryId"`
+	AdvisoryTitle  string      `json:"advisoryTitle"`
+	BugIDs         []string    `json:"bugIDs"`
+	CsafURL        string      `json:"csafUrl"`
+	Cves           []string    `json:"cves"`
+	CvrfURL        string      `json:"cvrfUrl"`
+	CvssBaseScore  string      `json:"cvssBaseScore"`
+	Cwe            []string    `json:"cwe"`
+	FirstPublished string      `json:"firstPublished"`
+	IpsSignatures  interface{} `json:"ipsSignatures"`
+	LastUpdated    string      `json:"lastUpdated"`
+	ProductNames   []string    `json:"productNames"`
+	PublicationURL string      `json:"publicationUrl"`
+	Sir            string      `json:"sir"`
+	Status         string      `json:"status"`
+	Summary        string      `json:"summary"`
+	Version        string      `json:"version"`
+}
