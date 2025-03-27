e56fd3abe7a13d8adf61548718d9eaa00133ccfe
MaineK00n <mainek00n.1229@gmail.com>
2025-03-28 04:30:29 +0900

    feat(cmd): add dotgit pull

--- pkg/cmd/root/root.go
+++ pkg/cmd/root/root.go
@@ -5,25 +5,26 @@ import (
 	"github.com/spf13/cobra"
 
 	diffCmd "github.com/MaineK00n/vuls-data-update/pkg/cmd/diff"
+	dotgitCmd "github.com/MaineK00n/vuls-data-update/pkg/cmd/dotgit"
 	extractCmd "github.com/MaineK00n/vuls-data-update/pkg/cmd/extract"
 	fetchCmd "github.com/MaineK00n/vuls-data-update/pkg/cmd/fetch"
 )
 
 func NewCmdRoot() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:           "vuls-data-update <command> <subcommand> ([os|library|other])",
+		Use:           "vuls-data-update <command>",
 		Short:         "Vuls data update",
-		Long:          "Fetch and Extract data source",
+		Long:          "Fetch and Extract data source, Operate vuls-data-* dotgit",
 		SilenceErrors: true,
 		SilenceUsage:  true,
 		Example: heredoc.Doc(`
-			$ vuls-data-update fetch debian-security-tracker
-			$ vuls-data-update fetch cargo-db
-			$ vuls-data-update fetch nvd-feed
+			$ vuls-data-update fetch debian-security-tracker-salsa
+			$ vuls-data-update extract debian-security-tracker-salsa vuls-data-raw-debian-security-tracker-salsa
+			$ vuls-data-update dotgit pull ghcr.io/vulsio/vuls-data-db:vuls-data-raw-debian-security-tracker-salsa
 		`),
 	}
 
-	cmd.AddCommand(fetchCmd.NewCmdFetch(), extractCmd.NewCmdExtract(), diffCmd.NewCmdDiff())
+	cmd.AddCommand(fetchCmd.NewCmdFetch(), extractCmd.NewCmdExtract(), dotgitCmd.NewCmdDotGit(), diffCmd.NewCmdDiff())
 
 	return cmd
 }
