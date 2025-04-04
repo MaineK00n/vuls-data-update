1981847d6fb891fbf00ec51406c84118a8a41636
MaineK00n <mainek00n.1229@gmail.com>
2025-04-04 10:25:31 +0900

    feat(fetch/wolfi): add wolfi-secdb

---
+++ pkg/fetch/wolfi/secdb/secdb_test.go
@@ -0,0 +1,80 @@
+package secdb_test
+
+import (
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/MaineK00n/vuls-data-update/pkg/fetch/wolfi/secdb"
+)
+
+func TestFetch(t *testing.T) {
+	tests := []struct {
+		name     string
+		hasError bool
+	}{
+		{
+			name: "happy",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				http.ServeFile(w, r, filepath.Join("testdata", "fixtures", tt.name, path.Base(r.URL.Path)))
+			}))
+			defer ts.Close() //nolint:errcheck
+
+			u, err := url.JoinPath(ts.URL, "os/security.json")
+			if err != nil {
+				t.Error("unexpected error:", err)
+			}
+
+			dir := t.TempDir()
+			err = secdb.Fetch(secdb.WithDataURL(u), secdb.WithDir(dir), secdb.WithRetry(0))
+			switch {
+			case err != nil && !tt.hasError:
+				t.Error("unexpected error:", err)
+			case err == nil && tt.hasError:
+				t.Error("expected error has not occurred")
+			}
+
+			if err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+				if err != nil {
+					return err
+				}
+
+				if d.IsDir() {
+					return nil
+				}
+
+				wantDir, wantFile := filepath.Split(strings.TrimPrefix(path, dir))
+				want, err := os.ReadFile(filepath.Join("testdata", "golden", wantDir, url.QueryEscape(wantFile)))
+				if err != nil {
+					return err
+				}
+
+				got, err := os.ReadFile(path)
+				if err != nil {
+					return err
+				}
+
+				if diff := cmp.Diff(want, got); diff != "" {
+					t.Errorf("Fetch(). (-expected +got):\n%s", diff)
+				}
+
+				return nil
+			}); err != nil {
+				t.Error("walk error:", err)
+			}
+		})
+	}
+}
