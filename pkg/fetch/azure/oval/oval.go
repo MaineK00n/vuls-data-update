6641611ddefa35a3c6402763bf6182f6440fb58b
MaineK00n <mainek00n.1229@gmail.com>
2025-04-04 14:14:48 +0900

    feat(fetch): add azure-oval

---
+++ pkg/fetch/azure/oval/oval.go
@@ -0,0 +1,171 @@
+package oval
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"path/filepath"
+	"strings"
+
+	"github.com/cheggaaa/pb/v3"
+	"github.com/pkg/errors"
+
+	"github.com/MaineK00n/vuls-data-update/pkg/fetch/util"
+	utilhttp "github.com/MaineK00n/vuls-data-update/pkg/fetch/util/http"
+)
+
+const dataURL = "https://github.com/microsoft/AzureLinuxVulnerabilityData/archive/refs/heads/main.tar.gz"
+
+type options struct {
+	dataURL string
+	dir     string
+	retry   int
+}
+
+type Option interface {
+	apply(*options)
+}
+
+type dataURLOption string
+
+func (u dataURLOption) apply(opts *options) {
+	opts.dataURL = string(u)
+}
+
+func WithDataURL(url string) Option {
+	return dataURLOption(url)
+}
+
+type dirOption string
+
+func (d dirOption) apply(opts *options) {
+	opts.dir = string(d)
+}
+
+func WithDir(dir string) Option {
+	return dirOption(dir)
+}
+
+type retryOption int
+
+func (r retryOption) apply(opts *options) {
+	opts.retry = int(r)
+}
+
+func WithRetry(retry int) Option {
+	return retryOption(retry)
+}
+
+func Fetch(opts ...Option) error {
+	options := &options{
+		dataURL: dataURL,
+		dir:     filepath.Join(util.CacheDir(), "fetch", "azure", "oval"),
+		retry:   3,
+	}
+
+	for _, o := range opts {
+		o.apply(options)
+	}
+
+	if err := util.RemoveAll(options.dir); err != nil {
+		return errors.Wrapf(err, "remove %s", options.dir)
+	}
+
+	log.Println("[INFO] Fetch Azure Linux OVAL")
+	resp, err := utilhttp.NewClient(utilhttp.WithClientRetryMax(options.retry)).Get(options.dataURL)
+	if err != nil {
+		return errors.Wrap(err, "fetch")
+	}
+	defer resp.Body.Close() //nolint:errcheck
+
+	if resp.StatusCode != http.StatusOK {
+		_, _ = io.Copy(io.Discard, resp.Body)
+		return errors.Errorf("error response with status code %d", resp.StatusCode)
+	}
+
+	gr, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		return errors.Wrap(err, "create gzip reader")
+	}
+	defer gr.Close() //nolint:errcheck
+
+	tr := tar.NewReader(gr)
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return errors.Wrap(err, "next tar reader")
+		}
+
+		if hdr.FileInfo().IsDir() || filepath.Ext(hdr.Name) != ".xml" {
+			continue
+		}
+
+		var root root
+		if err := xml.NewDecoder(tr).Decode(&root); err != nil {
+			return errors.Wrap(err, "decode xml")
+		}
+
+		var t, v string
+		switch {
+		case strings.HasPrefix(filepath.Base(hdr.Name), "azurelinux-"):
+			t = "azurelinux"
+			v = strings.TrimPrefix(strings.TrimSuffix(filepath.Base(hdr.Name), "-oval.xml"), "azurelinux-")
+		case strings.HasPrefix(filepath.Base(hdr.Name), "cbl-mariner-"):
+			t = "cbl-mariner"
+			v = strings.TrimPrefix(strings.TrimSuffix(filepath.Base(hdr.Name), "-oval.xml"), "cbl-mariner-")
+		default:
+			return errors.Errorf("unexpected oval file name. expected: %q, actual: %q", []string{"azurelinux-<version>-oval.xml", "cbl-mariner-<version>-oval.xml"}, filepath.Base(hdr.Name))
+		}
+
+		log.Printf("[INFO] Fetch %s %s OVAL", t, v)
+
+		log.Printf("[INFO] Fetch %s %s Definitions", t, v)
+		bar := pb.StartNew(len(root.Definitions.Definition))
+		for _, def := range root.Definitions.Definition {
+			if err := util.Write(filepath.Join(options.dir, t, v, "definitions", fmt.Sprintf("%s.json", def.ID)), def); err != nil {
+				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, t, v, "definitions", fmt.Sprintf("%s.json", def.ID)))
+			}
+			bar.Increment()
+		}
+		bar.Finish()
+
+		log.Printf("[INFO] Fetch %s %s Tests", t, v)
+		bar = pb.StartNew(len(root.Tests.RpminfoTest))
+		for _, test := range root.Tests.RpminfoTest {
+			if err := util.Write(filepath.Join(options.dir, t, v, "tests", "rpminfo_test", fmt.Sprintf("%s.json", test.ID)), test); err != nil {
+				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, t, v, "tests", "rpminfo_test", fmt.Sprintf("%s.json", test.ID)))
+			}
+			bar.Increment()
+		}
+		bar.Finish()
+
+		log.Printf("[INFO] Fetch %s %s Objects", t, v)
+		bar = pb.StartNew(len(root.Objects.RpminfoObject))
+		for _, object := range root.Objects.RpminfoObject {
+			if err := util.Write(filepath.Join(options.dir, t, v, "objects", "rpminfo_object", fmt.Sprintf("%s.json", object.ID)), object); err != nil {
+				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, t, v, "objects", "rpminfo_object", fmt.Sprintf("%s.json", object.ID)))
+			}
+			bar.Increment()
+		}
+		bar.Finish()
+
+		log.Printf("[INFO] Fetch %s %s States", t, v)
+		bar = pb.StartNew(len(root.States.RpminfoState))
+		for _, state := range root.States.RpminfoState {
+			if err := util.Write(filepath.Join(options.dir, t, v, "states", "rpminfo_state", fmt.Sprintf("%s.json", state.ID)), state); err != nil {
+				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, t, v, "states", "rpminfo_state", fmt.Sprintf("%s.json", state.ID)))
+			}
+			bar.Increment()
+		}
+		bar.Finish()
+	}
+
+	return nil
+}
---
+++ pkg/fetch/azure/oval/types.go
@@ -0,0 +1,90 @@
+package oval
+
+import "encoding/xml"
+
+type root struct {
+	XMLName        xml.Name `xml:"oval_definitions" json:"oval_definitions,omitempty"`
+	Xmlns          string   `xml:"xmlns,attr" json:"xmlns,omitempty"`
+	Oval           string   `xml:"oval,attr" json:"oval,omitempty"`
+	LinuxDef       string   `xml:"linux-def,attr" json:"linux-def,omitempty"`
+	Xsi            string   `xml:"xsi,attr" json:"xsi,omitempty"`
+	SchemaLocation string   `xml:"schemaLocation,attr" json:"schemalocation,omitempty"`
+	Generator      struct {
+		ProductName    string `xml:"product_name"`
+		ProductVersion string `xml:"product_version"`
+		SchemaVersion  string `xml:"schema_version"`
+		Timestamp      string `xml:"timestamp"`
+		ContentVersion string `xml:"content_version"`
+	} `xml:"generator" json:"generator,omitempty"`
+	Definitions struct {
+		Definition []Definition `xml:"definition" json:"definition,omitempty"`
+	} `xml:"definitions" json:"definitions,omitempty"`
+	Tests struct {
+		RpminfoTest []RpminfoTest `xml:"rpminfo_test" json:"rpminfo_test,omitempty"`
+	} `xml:"tests" json:"tests,omitempty"`
+	Objects struct {
+		RpminfoObject []RpminfoObject `xml:"rpminfo_object" json:"rpminfo_object,omitempty"`
+	} `xml:"objects" json:"objects,omitempty"`
+	States struct {
+		RpminfoState []RpminfoState `xml:"rpminfo_state" json:"rpminfo_state,omitempty"`
+	} `xml:"states" json:"states,omitempty"`
+}
+
+type Definition struct {
+	Class    string `xml:"class,attr" json:"class,omitempty"`
+	ID       string `xml:"id,attr" json:"id,omitempty"`
+	Version  string `xml:"version,attr" json:"version,omitempty"`
+	Metadata struct {
+		Title    string `xml:"title"`
+		Affected struct {
+			Family   string `xml:"family,attr" json:"family,omitempty"`
+			Platform string `xml:"platform"`
+		} `xml:"affected" json:"affected,omitempty"`
+		Reference struct {
+			RefID  string `xml:"ref_id,attr" json:"ref_id,omitempty"`
+			RefURL string `xml:"ref_url,attr" json:"ref_url,omitempty"`
+			Source string `xml:"source,attr" json:"source,omitempty"`
+		} `xml:"reference" json:"reference,omitempty"`
+		Patchable    string `xml:"patchable"`
+		AdvisoryDate string `xml:"advisory_date"`
+		AdvisoryID   string `xml:"advisory_id"`
+		Severity     string `xml:"severity"`
+		Description  string `xml:"description"`
+	} `xml:"metadata" json:"metadata,omitempty"`
+	Criteria struct {
+		Operator  string `xml:"operator,attr" json:"operator,omitempty"`
+		Criterion []struct {
+			Comment string `xml:"comment,attr" json:"comment,omitempty"`
+			TestRef string `xml:"test_ref,attr" json:"test_ref,omitempty"`
+		} `xml:"criterion" json:"criterion,omitempty"`
+	} `xml:"criteria" json:"criteria,omitempty"`
+}
+
+type RpminfoTest struct {
+	Check   string `xml:"check,attr" json:"check,omitempty"`
+	Comment string `xml:"comment,attr" json:"comment,omitempty"`
+	ID      string `xml:"id,attr" json:"id,omitempty"`
+	Version string `xml:"version,attr" json:"version,omitempty"`
+	Object  struct {
+		ObjectRef string `xml:"object_ref,attr" json:"object_ref,omitempty"`
+	} `xml:"object" json:"object,omitempty"`
+	State struct {
+		StateRef string `xml:"state_ref,attr" json:"state_ref,omitempty"`
+	} `xml:"state" json:"state,omitempty"`
+}
+
+type RpminfoObject struct {
+	ID      string `xml:"id,attr" json:"id,omitempty"`
+	Version string `xml:"version,attr" json:"version,omitempty"`
+	Name    string `xml:"name"`
+}
+
+type RpminfoState struct {
+	ID      string `xml:"id,attr" json:"id,omitempty"`
+	Version string `xml:"version,attr" json:"version,omitempty"`
+	Evr     struct {
+		Text      string `xml:",chardata" json:"text,omitempty"`
+		Datatype  string `xml:"datatype,attr" json:"datatype,omitempty"`
+		Operation string `xml:"operation,attr" json:"operation,omitempty"`
+	} `xml:"evr" json:"evr,omitempty"`
+}
