14501048c23ad1562155b94f6ec18b9dee8ca34b
MaineK00n <mainek00n.1229@gmail.com>
2025-04-08 10:47:49 +0900

    feat(cmd/dotgit): add dotgit contains (#385)

---
+++ pkg/dotgit/contains/contains.go
@@ -0,0 +1,41 @@
+package contains
+
+import (
+	"fmt"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/pkg/errors"
+)
+
+type CommitNotFoundError struct {
+	Repository string
+	Commit     string
+	Err        error
+}
+
+func (e *CommitNotFoundError) Error() string {
+	return fmt.Sprintf("commit %s not found in %s: %v", e.Commit, e.Repository, e.Err)
+}
+
+func (e *CommitNotFoundError) Unwrap() error { return e.Err }
+
+func Contains(repository, commit string) error {
+	r, err := git.PlainOpen(repository)
+	if err != nil {
+		return errors.Wrapf(err, "open %s", repository)
+	}
+
+	if _, err := r.CommitObject(plumbing.NewHash(commit)); err != nil {
+		if errors.Is(err, plumbing.ErrObjectNotFound) {
+			return &CommitNotFoundError{
+				Repository: repository,
+				Commit:     commit,
+				Err:        err,
+			}
+		}
+		return errors.Wrap(err, "get commit")
+	}
+
+	return nil
+}
