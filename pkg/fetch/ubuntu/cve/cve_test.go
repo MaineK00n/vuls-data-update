406f8fafb714a739d9202fbf6b082c873263627b
MaineK00n <mainek00n.1229@gmail.com>
2025-04-10 12:31:27 +0900

    feat(fetch/ubuntu): add ubuntu-(cve|notice) (#372)

---
+++ pkg/fetch/ubuntu/cve/cve_test.go
@@ -0,0 +1,74 @@
+package cve_test
+
+import (
+	"fmt"
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/MaineK00n/vuls-data-update/pkg/fetch/ubuntu/cve"
+)
+
+func TestFetch(t *testing.T) {
+	tests := []struct {
+		name     string
+		hasError bool
+	}{
+		{
+			name: "happy",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				http.ServeFile(w, r, filepath.Join("testdata", "fixtures", tt.name, fmt.Sprintf("%s_%s.json", r.URL.Query().Get("offset"), r.URL.Query().Get("limit"))))
+			}))
+			defer ts.Close() //nolint:errcheck
+
+			dir := t.TempDir()
+			err := cve.Fetch(cve.WithBaseURL(fmt.Sprintf("%s/security/cves.json?limit=%%d&offset=%%d&order=oldest&show_hidden=true", ts.URL)), cve.WithDir(dir), cve.WithRetry(0), cve.WithConcurrency(1), cve.WithWait(0))
+			switch {
+			case err != nil && !tt.hasError:
+				t.Error("unexpected error:", err)
+			case err == nil && tt.hasError:
+				t.Error("expected error has not occurred")
+			}
+
+			if err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+				if err != nil {
+					return err
+				}
+
+				if d.IsDir() {
+					return nil
+				}
+
+				wantDir, wantFile := filepath.Split(strings.TrimPrefix(path, dir))
+				want, err := os.ReadFile(filepath.Join("testdata", "golden", wantDir, url.QueryEscape(wantFile)))
+				if err != nil {
+					return err
+				}
+
+				got, err := os.ReadFile(path)
+				if err != nil {
+					return err
+				}
+
+				if diff := cmp.Diff(want, got); diff != "" {
+					t.Errorf("Fetch(). (-expected +got):\n%s", diff)
+				}
+
+				return nil
+			}); err != nil {
+				t.Error("walk error:", err)
+			}
+		})
+	}
+}
---
+++ pkg/fetch/ubuntu/notice/notice.go
@@ -0,0 +1,178 @@
+package notice
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"path/filepath"
+	"strings"
+
+	"github.com/hashicorp/go-retryablehttp"
+	"github.com/pkg/errors"
+
+	"github.com/MaineK00n/vuls-data-update/pkg/fetch/util"
+	utilhttp "github.com/MaineK00n/vuls-data-update/pkg/fetch/util/http"
+)
+
+const baseURL = "https://ubuntu.com/security/notices.json?limit=%d&offset=%d&order=oldest&show_hidden=true"
+
+type options struct {
+	baseURL     string
+	dir         string
+	retry       int
+	concurrency int
+	wait        int
+}
+
+type Option interface {
+	apply(*options)
+}
+
+type baseURLOption string
+
+func (u baseURLOption) apply(opts *options) {
+	opts.baseURL = string(u)
+}
+
+func WithBaseURL(url string) Option {
+	return baseURLOption(url)
+}
+
+type dirOption string
+
+func (d dirOption) apply(opts *options) {
+	opts.dir = string(d)
+}
+
+func WithDir(dir string) Option {
+	return dirOption(dir)
+}
+
+type retryOption int
+
+func (r retryOption) apply(opts *options) {
+	opts.retry = int(r)
+}
+
+func WithRetry(retry int) Option {
+	return retryOption(retry)
+}
+
+type concurrencyOption int
+
+func (r concurrencyOption) apply(opts *options) {
+	opts.concurrency = int(r)
+}
+
+func WithConcurrency(concurrency int) Option {
+	return concurrencyOption(concurrency)
+}
+
+type waitOption int
+
+func (r waitOption) apply(opts *options) {
+	opts.wait = int(r)
+}
+
+func WithWait(wait int) Option {
+	return waitOption(wait)
+}
+
+func Fetch(opts ...Option) error {
+	options := &options{
+		baseURL:     baseURL,
+		dir:         filepath.Join(util.CacheDir(), "fetch", "ubuntu", "notice"),
+		retry:       10,
+		concurrency: 10,
+		wait:        1,
+	}
+
+	for _, o := range opts {
+		o.apply(options)
+	}
+
+	if err := util.RemoveAll(options.dir); err != nil {
+		return errors.Wrapf(err, "remove %s", options.dir)
+	}
+
+	log.Println("[INFO] Fetch Ubuntu Notices")
+	client := utilhttp.NewClient(utilhttp.WithClientRetryMax(options.retry))
+
+	header := make(http.Header)
+	header.Set("Accept", "application/json")
+
+	req, err := utilhttp.NewRequest(http.MethodGet, fmt.Sprintf(options.baseURL, 1, 0), utilhttp.WithRequestHeader(header))
+	if err != nil {
+		return errors.Wrap(err, "new request")
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return errors.Wrap(err, "fetch notices")
+	}
+	defer resp.Body.Close() //nolint:errcheck
+
+	if resp.StatusCode != http.StatusOK {
+		_, _ = io.Copy(io.Discard, resp.Body)
+		return errors.Errorf("error response with status code %d", resp.StatusCode)
+	}
+
+	var r response
+	if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+		return errors.Wrap(err, "decode json")
+	}
+
+	var reqs []*retryablehttp.Request
+	for i := 0; i < r.TotalResults; i += 20 {
+		req, err := utilhttp.NewRequest(http.MethodGet, fmt.Sprintf(options.baseURL, 20, i), utilhttp.WithRequestHeader(header))
+		if err != nil {
+			return errors.Wrap(err, "new request")
+		}
+		reqs = append(reqs, req)
+	}
+
+	if err := client.PipelineDo(reqs, options.concurrency, options.wait, func(resp *http.Response) error {
+		defer resp.Body.Close() //nolint:errcheck
+
+		if resp.StatusCode != http.StatusOK {
+			_, _ = io.Copy(io.Discard, resp.Body)
+			return errors.Errorf("error response with status code %d", resp.StatusCode)
+		}
+
+		var r response
+		if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+			return errors.Wrap(err, "decode json")
+		}
+
+		for _, n := range r.Notices {
+			switch {
+			case strings.HasPrefix(n.ID, "USN"), strings.HasPrefix(n.ID, "LSN"), strings.HasPrefix(n.ID, "SSN"):
+				splitted, err := util.Split(n.ID, "-", "-")
+				if err != nil {
+					return errors.Errorf("unexpected ID format. expected: %q, actual: %q", "(USN|LSN|SSN)-\\d{1,5}-\\d{1,2}", n.ID)
+				}
+
+				if err := util.Write(filepath.Join(options.dir, splitted[0], splitted[1], fmt.Sprintf("%s.json", n.ID)), n); err != nil {
+					return errors.Wrapf(err, "write %s", filepath.Join(options.dir, splitted[0], splitted[1], fmt.Sprintf("%s.json", n.ID)))
+				}
+			default:
+				splitted, err := util.Split(n.ID, "-")
+				if err != nil {
+					return errors.Errorf("unexpected ID format. expected: %q, actual: %q", "\\d{1,5}-\\d{1,2}", n.ID)
+				}
+
+				if err := util.Write(filepath.Join(options.dir, "UNKNWON", splitted[0], fmt.Sprintf("%s.json", n.ID)), n); err != nil {
+					return errors.Wrapf(err, "write %s", filepath.Join(options.dir, "UNKNWON", splitted[0], fmt.Sprintf("%s.json", n.ID)))
+				}
+			}
+		}
+
+		return nil
+	}); err != nil {
+		return errors.Wrap(err, "pipeline do")
+	}
+
+	return nil
+}
