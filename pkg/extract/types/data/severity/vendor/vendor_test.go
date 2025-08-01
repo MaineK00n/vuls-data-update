46ad6ddadd552f0e887eefebe1857cd69a6077d1
MaineK00n <mainek00n.1229@gmail.com>
2025-08-01 19:55:51 +0900

    feat(types/data/severity): add vendorTypes.Compare

---
+++ pkg/extract/types/data/severity/vendor/vendor_test.go
@@ -0,0 +1,25 @@
+package vendor
+
+import "testing"
+
+func TestCompare(t *testing.T) {
+	type args struct {
+		source string
+		x      string
+		y      string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Compare(tt.args.source, tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("Compare() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
