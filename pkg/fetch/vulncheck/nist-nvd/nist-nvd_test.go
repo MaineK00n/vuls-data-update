a1f72d8bf2a161709c90fa8d393243e54e7b4cff
MaineK00n <mainek00n.1229@gmail.com>
2025-08-08 06:25:44 +0900

    feat(fetch/vulncheck): add nist-nvd

---
+++ pkg/fetch/vulncheck/nist-nvd/nist-nvd_test.go
@@ -0,0 +1,154 @@
+package nistnvd_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+
+	nistnvd "github.com/MaineK00n/vuls-data-update/pkg/fetch/vulncheck/nist-nvd"
+)
+
+func TestFetch(t *testing.T) {
+	type args struct {
+		apiToken string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		hasError bool
+	}{
+		{
+			name: "happy",
+			args: args{
+				apiToken: "vulncheck_token",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				switch {
+				case strings.HasSuffix(r.URL.Path, "backup/nist-nvd"):
+					c, err := r.Cookie("token")
+					if err != nil {
+						http.Error(w, fmt.Sprintf("Bad Request. err: %s", err), http.StatusBadRequest)
+						return
+					}
+					if c.Value != "vulncheck_token" {
+						http.Error(w, "Unauthorized", http.StatusUnauthorized)
+						return
+					}
+
+					bs, err := json.Marshal(struct {
+						Benchmark float64 `json:"_benchmark"`
+						Meta      struct {
+							Timestamp time.Time `json:"timestamp"`
+							Index     string    `json:"index"`
+						} `json:"_meta"`
+						Data []struct {
+							Filename      string    `json:"filename"`
+							Sha256        string    `json:"sha256"`
+							DateAdded     time.Time `json:"date_added"`
+							URL           string    `json:"url"`
+							URLTTLMinutes int       `json:"url_ttl_minutes"`
+							URLExpires    time.Time `json:"url_expires"`
+						} `json:"data"`
+					}{
+						Benchmark: 0.035335,
+						Meta: struct {
+							Timestamp time.Time `json:"timestamp"`
+							Index     string    `json:"index"`
+						}{
+							Timestamp: time.Now(),
+							Index:     "nist-nvd",
+						},
+						Data: []struct {
+							Filename      string    `json:"filename"`
+							Sha256        string    `json:"sha256"`
+							DateAdded     time.Time `json:"date_added"`
+							URL           string    `json:"url"`
+							URLTTLMinutes int       `json:"url_ttl_minutes"`
+							URLExpires    time.Time `json:"url_expires"`
+						}{
+							{
+								Filename:      "nist-nvd-1754583421218404458.zip",
+								Sha256:        "04f071a6ef1b39b9a115425b1b5a3add18571768310cae12f578d4b177bf4de8",
+								DateAdded:     time.Now(),
+								URL:           fmt.Sprintf("http://%s/nist-nvd-1754583421218404458.zip", r.Host),
+								URLTTLMinutes: 15,
+								URLExpires:    time.Now(),
+							},
+						},
+					})
+					if err != nil {
+						http.Error(w, fmt.Sprintf("Internal Server Error. err: %s", err), http.StatusInternalServerError)
+						return
+					}
+					http.ServeContent(w, r, "", time.Now(), bytes.NewReader(bs))
+				default:
+					http.ServeFile(w, r, filepath.Join("testdata", "fixtures", tt.name, path.Base(r.URL.Path)))
+				}
+			}))
+			defer ts.Close()
+
+			u, err := url.JoinPath(ts.URL, "v3/backup/nist-nvd")
+			if err != nil {
+				t.Error("unexpected error:", err)
+			}
+
+			dir := t.TempDir()
+			err = nistnvd.Fetch(tt.args.apiToken, nistnvd.WithBaseURL(u), nistnvd.WithDir(dir), nistnvd.WithRetry(0))
+			switch {
+			case err != nil && !tt.hasError:
+				t.Error("unexpected error:", err)
+			case err == nil && tt.hasError:
+				t.Error("expected error has not occurred")
+			default:
+				if err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+					if err != nil {
+						return err
+					}
+
+					if d.IsDir() {
+						return nil
+					}
+
+					rpath, err := filepath.Rel(dir, path)
+					if err != nil {
+						return err
+					}
+
+					want, err := os.ReadFile(filepath.Join("testdata", "golden", rpath))
+					if err != nil {
+						return err
+					}
+
+					got, err := os.ReadFile(path)
+					if err != nil {
+						return err
+					}
+
+					if diff := cmp.Diff(want, got); diff != "" {
+						t.Errorf("Fetch(). (-expected +got):\n%s", diff)
+					}
+
+					return nil
+				}); err != nil {
+					t.Error("walk error:", err)
+				}
+			}
+		})
+	}
+}
---
+++ pkg/fetch/vulncheck/nist-nvd/types.go
@@ -0,0 +1,228 @@
+package nistnvd
+
+import "time"
+
+type backupResponse struct {
+	Benchmark float64 `json:"_benchmark"`
+	Meta      struct {
+		Timestamp time.Time `json:"timestamp"`
+		Index     string    `json:"index"`
+	} `json:"_meta"`
+	Data []struct {
+		Filename      string    `json:"filename"`
+		Sha256        string    `json:"sha256"`
+		DateAdded     time.Time `json:"date_added"`
+		URL           string    `json:"url"`
+		URLTTLMinutes int       `json:"url_ttl_minutes"`
+		URLExpires    time.Time `json:"url_expires"`
+	} `json:"data"`
+}
+
+type doc struct {
+	CVEDataType         string    `json:"CVE_data_type"`
+	CVEDataFormat       string    `json:"CVE_data_format"`
+	CVEDataVersion      string    `json:"CVE_data_version"`
+	CVEDataNumberOfCVEs string    `json:"CVE_data_numberOfCVEs"`
+	CVEDataTimestamp    string    `json:"CVE_data_timestamp"`
+	CVEItems            []CVEItem `json:"CVE_Items"`
+}
+
+type CVEItem struct {
+	Cve              CVE            `json:"cve,omitempty"`
+	Configurations   Configurations `json:"configurations,omitempty"`
+	VCConfigurations Configurations `json:"vcConfigurations,omitempty"`
+	VCVulnerableCPEs []string       `json:"vcVulnerableCPEs,omitempty"`
+	Impact           Impact         `json:"impact,omitempty"`
+	PublishedDate    string         `json:"publishedDate,omitempty"`
+	LastModifiedDate string         `json:"lastModifiedDate,omitempty"`
+}
+
+type CVE struct {
+	DataType    string `json:"data_type,omitempty"`
+	DataFormat  string `json:"data_format,omitempty"`
+	DataVersion string `json:"data_version,omitempty"`
+	CVEDataMeta struct {
+		ID       string `json:"ID"`
+		ASSIGNER string `json:"ASSIGNER"`
+		STATE    string `json:"STATE,omitempty"`
+	} `json:"CVE_data_meta,omitempty"`
+	Affects struct {
+		Vendor struct {
+			VendorData []struct {
+				VendorName string `json:"vendor_name"`
+				Product    struct {
+					ProductData []struct {
+						ProductName string `json:"product_name"`
+						Version     struct {
+							VersionData []struct {
+								VersionValue    string `json:"version_value"`
+								VersionAffected string `json:"version_affected,omitempty"`
+							} `json:"version_data"`
+						} `json:"version"`
+					} `json:"product_data"`
+				} `json:"product"`
+			} `json:"vendor_data"`
+		} `json:"vendor"`
+	} `json:"affects,omitempty"`
+	Problemtype struct {
+		ProblemtypeData []struct {
+			Description []struct {
+				Lang  string `json:"lang"`
+				Value string `json:"value"`
+			} `json:"description"`
+		} `json:"problemtype_data"`
+	} `json:"problemtype"`
+	References struct {
+		ReferenceData []struct {
+			URL       string   `json:"url"`
+			Name      string   `json:"name,omitempty"`
+			Refsource string   `json:"refsource,omitempty"`
+			Tags      []string `json:"tags,omitempty"`
+		} `json:"reference_data"`
+	} `json:"references"`
+	Description struct {
+		DescriptionData []struct {
+			Lang  string `json:"lang"`
+			Value string `json:"value"`
+		} `json:"description_data"`
+	} `json:"description"`
+}
+
+type Configurations struct {
+	CVEDataVersion string `json:"CVE_data_version"`
+	Nodes          []Node `json:"nodes,omitempty"`
+}
+
+type Node struct {
+	Operator string     `json:"operator,omitempty"`
+	Negate   bool       `json:"negate,omitempty"`
+	Children []Node     `json:"children,omitempty"`
+	CpeMatch []CPEMatch `json:"cpe_match,omitempty"`
+}
+
+type CPEMatch struct {
+	Cpe22URI              string  `json:"cpe22Uri,omitempty"`
+	Cpe23URI              string  `json:"cpe23Uri"`
+	VersionEndExcluding   *string `json:"versionEndExcluding,omitempty"`
+	VersionEndIncluding   *string `json:"versionEndIncluding,omitempty"`
+	VersionStartExcluding *string `json:"versionStartExcluding,omitempty"`
+	VersionStartIncluding *string `json:"versionStartIncluding,omitempty"`
+	Vulnerable            bool    `json:"vulnerable"`
+	CPEName               []struct {
+		Cpe22URI         string `json:"cpe22Uri,omitempty"`
+		Cpe23URI         string `json:"cpe23Uri"`
+		LastModifiedDate string `json:"lastModifiedDate,omitempty"`
+	} `json:"cpe_name,omitempty"`
+}
+
+type Impact struct {
+	BaseMetricV2 *struct {
+		CvssV2 struct {
+			Version                    string  `json:"version"`
+			VectorString               string  `json:"vectorString"`
+			AccessVector               string  `json:"accessVector,omitempty"`
+			AccessComplexity           string  `json:"accessComplexity,omitempty"`
+			Authentication             string  `json:"authentication,omitempty"`
+			ConfidentialityImpact      string  `json:"confidentialityImpact,omitempty"`
+			IntegrityImpact            string  `json:"integrityImpact,omitempty"`
+			AvailabilityImpact         string  `json:"availabilityImpact,omitempty"`
+			BaseScore                  float64 `json:"baseScore"`
+			Exploitability             string  `json:"exploitability,omitempty"`
+			RemediationLevel           string  `json:"remediationLevel,omitempty"`
+			ReportConfidence           string  `json:"reportConfidence,omitempty"`
+			TemporalScore              float64 `json:"temporalScore,omitempty"`
+			CollateralDamagePotential  string  `json:"collateralDamagePotential,omitempty"`
+			TargetDistribution         string  `json:"targetDistribution,omitempty"`
+			ConfidentialityRequirement string  `json:"confidentialityRequirement,omitempty"`
+			IntegrityRequirement       string  `json:"integrityRequirement,omitempty"`
+			AvailabilityRequirement    string  `json:"availabilityRequirement,omitempty"`
+			EnvironmentalScore         float64 `json:"environmentalScore,omitempty"`
+		} `json:"cvssV2,omitempty"`
+		Severity                string  `json:"severity,omitempty"`
+		ExploitabilityScore     float64 `json:"exploitabilityScore,omitempty"`
+		ImpactScore             float64 `json:"impactScore,omitempty"`
+		AcInsufInfo             bool    `json:"acInsufInfo,omitempty"`
+		ObtainAllPrivilege      bool    `json:"obtainAllPrivilege,omitempty"`
+		ObtainUserPrivilege     bool    `json:"obtainUserPrivilege,omitempty"`
+		ObtainOtherPrivilege    bool    `json:"obtainOtherPrivilege,omitempty"`
+		UserInteractionRequired bool    `json:"userInteractionRequired,omitempty"`
+	} `json:"baseMetricV2,omitempty"`
+	BaseMetricV3 *struct {
+		CvssV3 struct {
+			Version                       string  `json:"version"`
+			VectorString                  string  `json:"vectorString,omitempty"`
+			AttackVector                  string  `json:"attackVector,omitempty"`
+			AttackComplexity              string  `json:"attackComplexity,omitempty"`
+			PrivilegesRequired            string  `json:"privilegesRequired,omitempty"`
+			UserInteraction               string  `json:"userInteraction,omitempty"`
+			Scope                         string  `json:"scope,omitempty"`
+			ConfidentialityImpact         string  `json:"confidentialityImpact,omitempty"`
+			IntegrityImpact               string  `json:"integrityImpact,omitempty"`
+			AvailabilityImpact            string  `json:"availabilityImpact,omitempty"`
+			BaseScore                     float64 `json:"baseScore"`
+			BaseSeverity                  string  `json:"baseSeverity"`
+			ExploitCodeMaturity           string  `json:"exploitCodeMaturity,omitempty"`
+			RemediationLevel              string  `json:"remediationLevel,omitempty"`
+			ReportConfidence              string  `json:"reportConfidence,omitempty"`
+			TemporalScore                 float64 `json:"temporalScore,omitempty"`
+			TemporalSeverity              string  `json:"temporalSeverity,omitempty"`
+			ConfidentialityRequirement    string  `json:"confidentialityRequirement,omitempty"`
+			IntegrityRequirement          string  `json:"integrityRequirement,omitempty"`
+			AvailabilityRequirement       string  `json:"availabilityRequirement,omitempty"`
+			ModifiedAttackVector          string  `json:"modifiedAttackVector,omitempty"`
+			ModifiedAttackComplexity      string  `json:"modifiedAttackComplexity,omitempty"`
+			ModifiedPrivilegesRequired    string  `json:"modifiedPrivilegesRequired,omitempty"`
+			ModifiedUserInteraction       string  `json:"modifiedUserInteraction,omitempty"`
+			ModifiedScope                 string  `json:"modifiedScope,omitempty"`
+			ModifiedConfidentialityImpact string  `json:"modifiedConfidentialityImpact,omitempty"`
+			ModifiedIntegrityImpact       string  `json:"modifiedIntegrityImpact,omitempty"`
+			ModifiedAvailabilityImpact    string  `json:"modifiedAvailabilityImpact,omitempty"`
+			EnvironmentalScore            float64 `json:"environmentalScore,omitempty"`
+			EnvironmentalSeverity         string  `json:"environmentalSeverity,omitempty"`
+		} `json:"cvssV3,omitempty"`
+		ExploitabilityScore float64 `json:"exploitabilityScore,omitempty"`
+		ImpactScore         float64 `json:"impactScore,omitempty"`
+	} `json:"baseMetricV3,omitempty"`
+	MetricV40 *struct {
+		Version                                 string  `json:"version"`
+		VectorString                            string  `json:"vectorString"`
+		BaseScore                               float64 `json:"baseScore"`
+		BaseSeverity                            string  `json:"baseSeverity"`
+		AttackVector                            string  `json:"attackVector,omitempty"`
+		AttackComplexity                        string  `json:"attackComplexity,omitempty"`
+		AttackRequirements                      string  `json:"attackRequirements,omitempty"`
+		PrivilegesRequired                      string  `json:"privilegesRequired,omitempty"`
+		UserInteraction                         string  `json:"userInteraction,omitempty"`
+		VulnerableSystemConfidentiality         string  `json:"vulnerableSystemConfidentiality,omitempty"`
+		VulnerableSystemIntegrity               string  `json:"vulnerableSystemIntegrity,omitempty"`
+		VulnerableSystemAvailability            string  `json:"vulnerableSystemAvailability,omitempty"`
+		SubsequentSystemConfidentiality         string  `json:"subsequentSystemConfidentiality,omitempty"`
+		SubsequentSystemIntegrity               string  `json:"subsequentSystemIntegrity,omitempty"`
+		SubsequentSystemAvailability            string  `json:"subsequentSystemAvailability,omitempty"`
+		ExploitMaturity                         string  `json:"exploitMaturity,omitempty"`
+		ConfidentialityRequirement              string  `json:"confidentialityRequirements,omitempty"`
+		IntegrityRequirement                    string  `json:"integrityRequirements,omitempty"`
+		AvailabilityRequirement                 string  `json:"availabilityRequirements,omitempty"`
+		ModifiedAttackVector                    string  `json:"modifiedAttackVector,omitempty"`
+		ModifiedAttackComplexity                string  `json:"modifiedAttackComplexity,omitempty"`
+		ModifiedAttackRequirements              string  `json:"modifiedAttackRequirements,omitempty"`
+		ModifiedPrivilegesRequired              string  `json:"modifiedPrivilegesRequired,omitempty"`
+		ModifiedUserInteraction                 string  `json:"modifiedUserInteraction,omitempty"`
+		ModifiedVulnerableSystemConfidentiality string  `json:"modifiedVulnerableSystemConfidentiality,omitempty"`
+		ModifiedVulnerableSystemIntegrity       string  `json:"modifiedVulnerableSystemIntegrity,omitempty"`
+		ModifiedVulnerableSystemAvailability    string  `json:"modifiedVulnerableSystemAvailability,omitempty"`
+		ModifiedSubsequentSystemConfidentiality string  `json:"modifiedSubsequentSystemConfidentiality,omitempty"`
+		ModifiedSubsequentSystemIntegrity       string  `json:"modifiedSubsequentSystemIntegrity,omitempty"`
+		ModifiedSubsequentSystemAvailability    string  `json:"modifiedSubsequentSystemAvailability,omitempty"`
+		Safety                                  string  `json:"safety,omitempty"`
+		Automatable                             string  `json:"automatable,omitempty"`
+		ProviderUrgency                         string  `json:"providerUrgency,omitempty"`
+		Recovery                                string  `json:"recovery,omitempty"`
+		ValueDensity                            string  `json:"valueDensity,omitempty"`
+		VulnerabilityResponseEffort             string  `json:"vulnerabilityResponseEffort,omitempty"`
+		ThreatScore                             float64 `json:"threatScore,omitempty"`
+		ThreatSeverity                          string  `json:"threatSeverity,omitempty"`
+		EnvironmentalScore                      float64 `json:"environmentalScore,omitempty"`
+		EnvironmentalSeverity                   string  `json:"environmentalSeverity,omitempty"`
+	} `json:"metricV40,omitempty"`
+}
