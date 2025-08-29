c8a9b407220432c5568bfa244cee5901316c83e4
MaineK00n <mainek00n.1229@gmail.com>
2025-08-29 14:40:29 +0900

    feat!(fetch/chainguard/secdb): split by package

--- pkg/fetch/chainguard/secdb/types.go
+++ pkg/fetch/chainguard/secdb/types.go
@@ -1,14 +1,16 @@
 package secdb
 
 type Advisory struct {
-	Apkurl    string   `json:"apkurl"`
-	Archs     []string `json:"archs"`
-	Reponame  string   `json:"reponame"`
-	Urlprefix string   `json:"urlprefix"`
-	Packages  []struct {
-		Pkg struct {
-			Name     string              `json:"name"`
-			Secfixes map[string][]string `json:"secfixes"`
-		} `json:"pkg"`
-	} `json:"packages"`
+	Apkurl    string    `json:"apkurl"`
+	Archs     []string  `json:"archs"`
+	Reponame  string    `json:"reponame"`
+	Urlprefix string    `json:"urlprefix"`
+	Packages  []Package `json:"packages"`
+}
+
+type Package struct {
+	Pkg struct {
+		Name     string              `json:"name"`
+		Secfixes map[string][]string `json:"secfixes"`
+	} `json:"pkg"`
 }
