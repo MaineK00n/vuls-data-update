aec5ce4f2f4ae95d751f27e9f7dd94207dd6bc56
MaineK00n <mainek00n.1229@gmail.com>
2025-08-29 14:35:56 +0900

    feat!(fetch,extract/alpine/secdb): split by package

--- pkg/fetch/alpine/secdb/types.go
+++ pkg/fetch/alpine/secdb/types.go
@@ -2,15 +2,17 @@ package secdb
 
 // Advisory represents a type included in files from the Alpine repository
 type Advisory struct {
-	Apkurl        string   `json:"apkurl"`
-	Archs         []string `json:"archs"`
-	Reponame      string   `json:"reponame"`
-	Urlprefix     string   `json:"urlprefix"`
-	Distroversion string   `json:"distroversion"`
-	Packages      []struct {
-		Pkg struct {
-			Name     string              `json:"name"`
-			Secfixes map[string][]string `json:"secfixes"`
-		} `json:"pkg"`
-	} `json:"packages"`
+	Apkurl        string    `json:"apkurl"`
+	Archs         []string  `json:"archs"`
+	Reponame      string    `json:"reponame"`
+	Urlprefix     string    `json:"urlprefix"`
+	Distroversion string    `json:"distroversion"`
+	Packages      []Package `json:"packages"`
+}
+
+type Package struct {
+	Pkg struct {
+		Name     string              `json:"name"`
+		Secfixes map[string][]string `json:"secfixes"`
+	} `json:"pkg"`
 }
