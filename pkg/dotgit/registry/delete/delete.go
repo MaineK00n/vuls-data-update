6f42197d47e8b4e53693f7b0957a9fa398df08fb
MaineK00n <mainek00n.1229@gmail.com>
2025-09-05 04:37:48 +0900

    feat(dotgit/registry): add delete cmd

---
+++ pkg/dotgit/registry/delete/delete.go
@@ -0,0 +1,164 @@
+package delete
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/pkg/errors"
+	"oras.land/oras-go/v2/registry/remote"
+
+	"github.com/MaineK00n/vuls-data-update/pkg/dotgit/registry/ls"
+	utilGitHub "github.com/MaineK00n/vuls-data-update/pkg/dotgit/registry/util/github"
+)
+
+type options struct {
+	apiEndpoint APIEndpoint
+}
+
+type Option interface {
+	apply(*options)
+}
+
+type apiOption struct {
+	APIEndpoint APIEndpoint
+}
+
+type APIEndpoint struct {
+	GitHub *GitHub
+}
+
+type GitHub struct {
+	BaseURL string
+	Type    string
+}
+
+func (a apiOption) apply(opts *options) {
+	opts.apiEndpoint = a.APIEndpoint
+}
+
+func WithAPIEndpoint(a APIEndpoint) Option {
+	return apiOption{APIEndpoint: a}
+}
+
+func Delete(image, token string, opts ...Option) error {
+	options := &options{
+		apiEndpoint: APIEndpoint{
+			GitHub: func() *GitHub {
+				if !strings.HasPrefix(image, "ghcr.io") {
+					return nil
+				}
+				return &GitHub{BaseURL: "https://api.github.com"}
+			}(),
+		},
+	}
+
+	for _, o := range opts {
+		o.apply(options)
+	}
+
+	repo, err := remote.NewRepository(image)
+	if err != nil {
+		return errors.Wrapf(err, "create client for %s", image)
+	}
+	if repo.Reference.Reference == "" {
+		return errors.Errorf("unexpected image format. expected: %q, actual: %q", []string{"<repository>@<digest>", "<repository>:<tag>@<digest>"}, image)
+	}
+
+	switch repo.Reference.Registry {
+	case "ghcr.io":
+		owner, pack, err := func() (string, string, error) {
+			switch repo.Reference.Registry {
+			case "ghcr.io":
+				lhs, rhs, ok := strings.Cut(repo.Reference.Repository, "/")
+				if !ok {
+					return "", "", errors.Errorf("unexpected repository format. expected: %q, actual: %q", "<registry>/<owner>/<package>", image)
+				}
+				return lhs, rhs, nil
+			default:
+				return "", "", nil
+			}
+		}()
+		if err != nil {
+			return errors.Wrap(err, "parse repository")
+		}
+
+		if options.apiEndpoint.GitHub == nil {
+			return errors.Errorf("GitHub API configuration is required for registry %q", repo.Reference.Registry)
+		}
+
+		if options.apiEndpoint.GitHub.Type == "" {
+			if err := utilGitHub.Do(http.MethodGet, fmt.Sprintf("%s/users/%s", options.apiEndpoint.GitHub.BaseURL, owner), token, func(resp *http.Response) error {
+				switch resp.StatusCode {
+				case http.StatusOK:
+					type users struct {
+						Type string `json:"type"`
+					}
+					var us users
+					if err := json.NewDecoder(resp.Body).Decode(&us); err != nil {
+						return errors.Wrap(err, "decode response")
+					}
+					switch us.Type {
+					case "Organization":
+						options.apiEndpoint.GitHub.Type = "orgs"
+					case "User":
+						options.apiEndpoint.GitHub.Type = "users"
+					default:
+						return errors.Errorf("unexpected repository type. expected: %q, actual: %s", []string{"Organization", "User"}, us.Type)
+					}
+					return nil
+				default:
+					return errors.Errorf("unexpected response status. expected: %d, actual: %d", []int{http.StatusOK}, resp.StatusCode)
+				}
+			}); err != nil {
+				return errors.Wrap(err, "call GitHub API")
+			}
+		}
+
+		rs, err := ls.List([]ls.Repository{{Type: options.apiEndpoint.GitHub.Type, Registry: repo.Reference.Registry, Owner: owner, Package: pack}}, token, ls.WithbaseURL(options.apiEndpoint.GitHub.BaseURL))
+		if err != nil {
+			return errors.Wrap(err, "list versions")
+		}
+
+		var ids []int
+		for _, r := range rs {
+			if r.Digest == repo.Reference.Reference {
+				ids = append(ids, r.ID)
+			}
+		}
+		if len(ids) == 0 {
+			return errors.Errorf("no matching digest: %q found in %s", repo.Reference.Reference, repo.Reference.Repository)
+		}
+
+		u, err := url.Parse(options.apiEndpoint.GitHub.BaseURL)
+		if err != nil {
+			return errors.Wrap(err, "parse url")
+		}
+		switch options.apiEndpoint.GitHub.Type {
+		case "orgs", "users":
+			for _, id := range ids {
+				uu := u.JoinPath(options.apiEndpoint.GitHub.Type, owner, "packages", "container", pack, "versions", fmt.Sprintf("%d", id))
+				if err := utilGitHub.Do(http.MethodDelete, uu.String(), token, func(resp *http.Response) error {
+					switch resp.StatusCode {
+					case http.StatusNoContent:
+						log.Printf("[INFO] Deleted: %s@%s (ID: %d)\n", repo.Reference.Repository, repo.Reference.Reference, id)
+						return nil
+					default:
+						return errors.Errorf("unexpected response status. expected: %d, actual: %d", []int{http.StatusNoContent}, resp.StatusCode)
+					}
+				}); err != nil {
+					return errors.Wrap(err, "call GitHub API")
+				}
+			}
+
+			return nil
+		default:
+			return errors.Errorf("unexpected registry type. expected: %q, actual: %q", []string{"orgs", "users"}, options.apiEndpoint.GitHub.Type)
+		}
+	default:
+		return errors.Errorf("unexpected registry. expected: %q, actual: %q", []string{"ghcr.io"}, repo.Reference.Registry)
+	}
+}
---
+++ pkg/dotgit/registry/delete/delete_test.go
@@ -0,0 +1,185 @@
+package delete_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"slices"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/MaineK00n/vuls-data-update/pkg/dotgit/registry/delete"
+	"github.com/MaineK00n/vuls-data-update/pkg/dotgit/registry/ls"
+)
+
+func TestDelete(t *testing.T) {
+	type args struct {
+		image string
+		token string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "happy",
+			args: args{
+				image: "ghcr.io/vulsio/vuls-data-db@sha256:6cb985595c5e29861b266ae89ac42946dc5451557b8fd949ad08df12f9615efb",
+				token: "token",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			vs := []ls.Version{
+				{
+					ID:             460898921,
+					Name:           "sha256:6413c62920ab027e680d7adf44bec195e9f2ec7a299140ff9a5d2193a626b673",
+					URL:            "https://api.github.com/orgs/vulsio/packages/container/vuls-data-db/versions/460898921",
+					PackageHTMLURL: "https://github.com/orgs/vulsio/packages/container/package/vuls-data-db",
+					CreatedAt:      "2025-07-14T13:05:30Z",
+					UpdatedAt:      "2025-07-14T13:05:30Z",
+					HTMLURL: func() *string {
+						s := "https://github.com/orgs/vulsio/packages/container/vuls-data-db/460898921"
+						return &s
+					}(),
+					Metadata: &ls.Metadata{
+						PackageType: "container",
+						Container: &ls.Container{
+							Tags: []string{"vuls-data-raw-suse-oval"},
+						},
+					},
+				},
+				{
+					ID:             460898773,
+					Name:           "sha256:6cb985595c5e29861b266ae89ac42946dc5451557b8fd949ad08df12f9615efb",
+					URL:            "https://api.github.com/orgs/vulsio/packages/container/vuls-data-db/versions/460898773",
+					PackageHTMLURL: "https://github.com/orgs/vulsio/packages/container/package/vuls-data-db",
+					CreatedAt:      "2025-07-14T13:05:21Z",
+					UpdatedAt:      "2025-07-14T13:05:21Z",
+					HTMLURL: func() *string {
+						s := "https://github.com/orgs/vulsio/packages/container/vuls-data-db/460898773"
+						return &s
+					}(),
+					Metadata: &ls.Metadata{
+						PackageType: "container",
+						Container: &ls.Container{
+							Tags: []string{"vuls-data-raw-ubuntu-vex"},
+						},
+					},
+				},
+			}
+
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				switch r.URL.Path {
+				case "/users/vulsio":
+					switch r.Method {
+					case http.MethodGet:
+						w.WriteHeader(http.StatusOK)
+						if _, err := w.Write([]byte(`{"login":"vulsio","type":"Organization"}`)); err != nil {
+							http.Error(w, fmt.Sprintf("unexpected error: %v", err), http.StatusInternalServerError)
+						}
+					default:
+						http.Error(w, "Bad Request", http.StatusBadRequest)
+					}
+				case "/orgs/vulsio/packages/container/vuls-data-db/versions":
+					switch r.Method {
+					case http.MethodGet:
+						perpage := func() int {
+							i, err := strconv.Atoi(r.URL.Query().Get("per_page"))
+							if err != nil {
+								return 30
+							}
+							if i < 1 || i > 100 {
+								return 30
+							}
+							return i
+						}()
+
+						page := func() int {
+							i, err := strconv.Atoi(r.URL.Query().Get("page"))
+							if err != nil {
+								return 1
+							}
+							if i < 1 {
+								return 1
+							}
+							return i
+						}()
+
+						w.Header().Set("link", func() string {
+							last := func() int {
+								if len(vs)%perpage == 0 {
+									return len(vs) / perpage
+								}
+								return len(vs)/perpage + 1
+							}()
+
+							var ss []string
+							if page < last {
+								ss = append(ss, fmt.Sprintf(`<https://api.github.com/organizations/54834211/packages/container/vuls-data-db/versions?page=%d&per_page=%d>; rel="next"`, page+1, perpage))
+							}
+							if page > 1 {
+								ss = append(ss, fmt.Sprintf(`<https://api.github.com/organizations/54834211/packages/container/vuls-data-db/versions?page=%d&per_page=%d>; rel="prev"`, page-1, perpage))
+							}
+							if page != 1 {
+								ss = append(ss, fmt.Sprintf(`<https://api.github.com/organizations/54834211/packages/container/vuls-data-db/versions?page=%d&per_page=%d>; rel="first"`, 1, perpage))
+							}
+							if page != last {
+								ss = append(ss, fmt.Sprintf(`<https://api.github.com/organizations/54834211/packages/container/vuls-data-db/versions?page=%d&per_page=%d>; rel="last"`, last, perpage))
+							}
+							return strings.Join(ss, ", ")
+						}())
+						w.WriteHeader(http.StatusOK)
+						if err := json.NewEncoder(w).Encode(func() []ls.Version {
+							start := (page - 1) * perpage
+							end := start + perpage
+
+							if start >= len(vs) {
+								return []ls.Version{}
+							}
+							if end > len(vs) {
+								return vs[start:]
+							}
+							return vs[start:end]
+						}()); err != nil {
+							http.Error(w, fmt.Sprintf("unexpected error: %v", err), http.StatusInternalServerError)
+						}
+					default:
+						http.Error(w, "Bad Request", http.StatusBadRequest)
+					}
+				case "/orgs/vulsio/packages/container/vuls-data-db/versions/460898773":
+					switch r.Method {
+					case http.MethodDelete:
+						vs = slices.DeleteFunc(vs, func(v ls.Version) bool {
+							return v.ID == 460898773
+						})
+						w.WriteHeader(http.StatusNoContent)
+					default:
+						http.Error(w, "Bad Request", http.StatusBadRequest)
+					}
+				default:
+					http.NotFound(w, r)
+				}
+			}))
+			defer ts.Close()
+
+			if err := delete.Delete(tt.args.image, tt.args.token, delete.WithAPIEndpoint(delete.APIEndpoint{GitHub: &delete.GitHub{BaseURL: ts.URL}})); (err != nil) != tt.wantErr {
+				t.Errorf("Delete() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			rs, err := ls.List([]ls.Repository{{Type: "orgs", Registry: "ghcr.io", Owner: "vulsio", Package: "vuls-data-db"}}, tt.args.token, ls.WithbaseURL(ts.URL))
+			if err != nil {
+				t.Errorf("List() error = %v", err)
+			}
+
+			if slices.ContainsFunc(rs, func(r ls.Response) bool { return strings.HasSuffix(tt.args.image, r.Digest) }) {
+				t.Errorf("Delete() seems not to have deleted the version with digest %q, remaining versions: %+v", tt.args.image, rs)
+			}
+		})
+	}
+}
