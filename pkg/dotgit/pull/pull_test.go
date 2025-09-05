15af5f6886695158e565b31ef63b5d57aecf7777
MaineK00n <mainek00n.1229@gmail.com>
2025-09-05 17:20:37 +0900

    feat(dotgit/pull): add registry, repo to pull dir (#552)

--- pkg/dotgit/pull/pull_test.go
+++ pkg/dotgit/pull/pull_test.go
@@ -2,7 +2,7 @@ package pull_test
 
 import (
 	"archive/tar"
-	"encoding/json"
+	"context"
 	"fmt"
 	"io"
 	"net/http"
@@ -10,151 +10,68 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
-	"strconv"
-	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/google/go-containerregistry/pkg/registry"
 	"github.com/klauspost/compress/zstd"
-	"github.com/opencontainers/go-digest"
-	"github.com/opencontainers/image-spec/specs-go"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/pkg/errors"
+	"oras.land/oras-go/v2"
+	"oras.land/oras-go/v2/registry/remote"
 
 	"github.com/MaineK00n/vuls-data-update/pkg/dotgit/pull"
 )
 
-func generateDescriptor(mediaType string, blob []byte) (desc ocispec.Descriptor) {
-	desc = ocispec.Descriptor{ // Generate descriptor based on the media type and blob content
-		MediaType: mediaType,
-		Digest:    digest.FromBytes(blob), // Calculate digest
-		Size:      int64(len(blob)),       // Include blob size
-	}
-	return desc
-}
-
-func generateManifestContent(config ocispec.Descriptor, layers ...ocispec.Descriptor) ([]byte, error) {
-	content := ocispec.Manifest{
-		Config:    config, // Set config blob
-		Layers:    layers, // Set layer blobs
-		Versioned: specs.Versioned{SchemaVersion: 2},
-	}
-	return json.Marshal(content) // Get json content
-}
-
 func TestPull(t *testing.T) {
 	type args struct {
-		tag  string
-		opts []pull.Option
+		repository string
+		opts       []pull.Option
 	}
 	tests := []struct {
-		name      string
-		args      args
-		layerBlob string
-		golden    string
-		hasError  bool
+		name     string
+		args     args
+		golden   string
+		hasError bool
 	}{
 		{
 			name: "vuls-data-raw-test restore: false",
 			args: args{
-				tag:  "vuls-data-raw-test",
-				opts: []pull.Option{pull.WithRestore(false)},
+				repository: "ghcr.io/vulsio/vuls-data-db:vuls-data-raw-test",
+				opts:       []pull.Option{pull.WithRestore(false)},
 			},
-			layerBlob: "testdata/fixtures/vuls-data-raw-test.tar.zst",
-			golden:    "testdata/golden/vuls-data-raw-test.tar.zst",
+			golden: "testdata/golden/vuls-data-raw-test.tar.zst",
 		},
 		{
 			name: "vuls-data-raw-test restore: true, native git",
 			args: args{
-				tag:  "vuls-data-raw-test",
-				opts: []pull.Option{pull.WithRestore(true), pull.WithUseNativeGit(true)},
+				repository: "ghcr.io/vulsio/vuls-data-db:vuls-data-raw-test",
+				opts:       []pull.Option{pull.WithRestore(true), pull.WithUseNativeGit(true)},
 			},
-			layerBlob: "testdata/fixtures/vuls-data-raw-test.tar.zst",
-			golden:    "testdata/golden/vuls-data-raw-test-restored.tar.zst",
+			golden: "testdata/golden/vuls-data-raw-test-restored.tar.zst",
 		},
 		{
 			name: "vuls-data-raw-test restore: true, go-git",
 			args: args{
-				tag:  "vuls-data-raw-test",
-				opts: []pull.Option{pull.WithRestore(true), pull.WithUseNativeGit(false)},
+				repository: "ghcr.io/vulsio/vuls-data-db:vuls-data-raw-test",
+				opts:       []pull.Option{pull.WithRestore(true), pull.WithUseNativeGit(false)},
 			},
-			layerBlob: "testdata/fixtures/vuls-data-raw-test.tar.zst",
-			golden:    "testdata/golden/vuls-data-raw-test-restored.tar.zst",
+			golden: "testdata/golden/vuls-data-raw-test-restored.tar.zst",
 		},
 		{
 			name: "vuls-data-raw-test-archive-1 restore: false",
 			args: args{
-				tag:  "vuls-data-raw-test-archive-1",
-				opts: []pull.Option{pull.WithRestore(false)},
+				repository: "ghcr.io/vulsio/vuls-data-db:vuls-data-raw-test-archive-1",
+				opts:       []pull.Option{pull.WithRestore(false)},
 			},
-			layerBlob: "testdata/fixtures/vuls-data-raw-test.tar.zst",
-			golden:    "testdata/golden/vuls-data-raw-test-archive-1.tar.zst",
+			golden: "testdata/golden/vuls-data-raw-test-archive-1.tar.zst",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			layerBlob, err := os.ReadFile(tt.layerBlob)
-			if err != nil {
-				t.Errorf("read %s. err: %v", tt.layerBlob, err)
-			}
-			layerDesc := generateDescriptor("application/vnd.vulsio.vuls-data-db.dotgit.layer.v1.tar+zstd", layerBlob)
-			configBlob := []byte("{}")
-			configDesc := generateDescriptor(ocispec.MediaTypeEmptyJSON, configBlob)
-			manifestBlob, err := generateManifestContent(configDesc, layerDesc)
-			if err != nil {
-				t.Errorf("generate a image manifest. err: %v", err)
-			}
-			manifestDesc := generateDescriptor(ocispec.MediaTypeImageManifest, manifestBlob)
-
-			ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				switch {
-				case r.URL.Path == "/v2/vuls-data-db/tags/list" && r.Method == "GET":
-					if err := json.NewEncoder(w).Encode(struct {
-						Tags []string `json:"tags"`
-					}{
-						Tags: []string{tt.args.tag},
-					}); err != nil {
-						t.Errorf("encode json. err: %v", err)
-					}
-				case strings.Contains(r.URL.Path, "/manifests/") && (r.Method == "HEAD" || r.Method == "GET"):
-					w.Header().Set("Content-Type", ocispec.MediaTypeImageManifest)
-					w.Header().Set("Docker-Content-Digest", string(manifestDesc.Digest))
-					w.Header().Set("Content-Length", strconv.Itoa(len([]byte(manifestBlob))))
-					if _, err := w.Write([]byte(manifestBlob)); err != nil {
-						t.Errorf("write manifest. err: %v", err)
-					}
-				case strings.Contains(r.URL.Path, "/blobs/") && (r.Method == "HEAD" || r.Method == "GET"):
-					ss := strings.Split(r.URL.Path, "/")
-					digest := ss[len(ss)-1]
-					var desc ocispec.Descriptor
-					var content []byte
-					switch digest {
-					case layerDesc.Digest.String():
-						desc = layerDesc
-						content = layerBlob
-					case configDesc.Digest.String():
-						desc = configDesc
-						content = configBlob
-					case manifestDesc.Digest.String():
-						desc = manifestDesc
-						content = manifestBlob
-					}
-					w.Header().Set("Content-Type", desc.MediaType)
-					w.Header().Set("Docker-Content-Digest", digest)
-					w.Header().Set("Content-Length", strconv.Itoa(len([]byte(content))))
-					if _, err := w.Write([]byte(content)); err != nil {
-						t.Errorf("write blob. err: %v", err)
-					}
-				default:
-					http.NotFound(w, r)
-				}
-			}))
+			ts := httptest.NewTLSServer(registry.New())
 			defer ts.Close()
-
-			u, err := url.Parse(ts.URL)
-			if err != nil {
-				t.Errorf("parse url. err: %v", err)
-			}
 
 			originalTransport := http.DefaultTransport
 			http.DefaultTransport = ts.Client().Transport
@@ -162,14 +79,23 @@ func TestPull(t *testing.T) {
 				http.DefaultTransport = originalTransport
 			}()
 
+			u, err := url.Parse(ts.URL)
+			if err != nil {
+				t.Fatalf("parse url. err: %v", err)
+			}
+
+			repo, err := remote.NewRepository(tt.args.repository)
+			if err != nil {
+				t.Fatalf("new repository: %v", err)
+			}
+			repo.Reference.Registry = u.Host
+
+			if err := setup(fmt.Sprintf("%s/%s", repo.Reference.Registry, repo.Reference.Repository)); err != nil {
+				t.Fatalf("setup: %v", err)
+			}
+
 			dir := t.TempDir()
-			err = pull.Pull(fmt.Sprintf("%s/vuls-data-db:%s", u.Host, tt.args.tag), func() []pull.Option {
-				opts := []pull.Option{
-					pull.WithDir(dir),
-				}
-				opts = append(opts, tt.args.opts...)
-				return opts
-			}()...)
+			err = pull.Pull(repo.Reference.String(), append([]pull.Option{pull.WithDir(dir)}, tt.args.opts...)...)
 			switch {
 			case err != nil && !tt.hasError:
 				t.Errorf("unexpected err: %v", err)
@@ -177,12 +103,12 @@ func TestPull(t *testing.T) {
 				t.Error("expected error has not occurred")
 			default:
 				var got []string
-				if err := filepath.WalkDir(dir, func(path string, d os.DirEntry, err error) error {
+				if err := filepath.WalkDir(filepath.Join(dir, u.Host, "vulsio", "vuls-data-db"), func(path string, d os.DirEntry, err error) error {
 					if err != nil {
 						return err
 					}
 
-					p, err := filepath.Rel(dir, path)
+					p, err := filepath.Rel(filepath.Join(dir, u.Host, "vulsio", "vuls-data-db"), path)
 					if err != nil {
 						return err
 					}
@@ -231,4 +157,42 @@ func TestPull(t *testing.T) {
 			}
 		})
 	}
+}
+
+func setup(url string) error {
+	ctx := context.TODO()
+
+	repo, err := remote.NewRepository(url)
+	if err != nil {
+		return errors.Wrapf(err, "create client for %s", url)
+	}
+
+	bs, err := os.ReadFile("testdata/fixtures/vuls-data-raw-test.tar.zst")
+	if err != nil {
+		return errors.Wrapf(err, "read %q", "testdata/fixtures/vuls-data-raw-test.tar.zst")
+	}
+
+	layerDescriptor, err := oras.PushBytes(ctx, repo, "application/vnd.vulsio.vuls-data-db.dotgit.layer.v1.tar+zstd", []byte(bs))
+	if err != nil {
+		return errors.Wrap(err, "push dotgit layer")
+	}
+	if layerDescriptor.Annotations == nil {
+		layerDescriptor.Annotations = make(map[string]string)
+	}
+	if _, ok := layerDescriptor.Annotations[ocispec.AnnotationTitle]; !ok {
+		layerDescriptor.Annotations[ocispec.AnnotationTitle] = "vuls-data-raw-test.tar.zst"
+	}
+
+	desc, err := oras.PackManifest(ctx, repo, oras.PackManifestVersion1_1, "application/vnd.vulsio.vuls-data-db.dotgit+type", oras.PackManifestOptions{Layers: []ocispec.Descriptor{layerDescriptor}})
+	if err != nil {
+		return errors.Wrap(err, "pack manifest")
+	}
+
+	for _, tag := range []string{"vuls-data-raw-test", "vuls-data-raw-test-archive-1"} {
+		if err := repo.Tag(ctx, desc, tag); err != nil {
+			return errors.Wrapf(err, "tagged %q for %+v", tag, desc)
+		}
+	}
+
+	return nil
 }
