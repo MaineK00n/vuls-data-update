2a8eeb4ec9eb4373b106ebf929e2e51170a23b32
MaineK00n <mainek00n.1229@gmail.com>
2025-11-17 13:09:55 +0900

    fix(fetch/photon/cve): support non CVE- prifix, duplicate IDs (#619)

--- pkg/fetch/photon/cve/types.go
+++ pkg/fetch/photon/cve/types.go
@@ -4,10 +4,11 @@ type versions struct {
 	Branches []string `json:"branches"`
 }
 
-type CVE struct {
-	AffVer   string  `json:"aff_ver"`
-	CveID    string  `json:"cve_id"`
-	CveScore float64 `json:"cve_score"`
+type cve struct {
+	CVEID    string  `json:"cve_id"`
 	Pkg      string  `json:"pkg"`
+	CVEScore float64 `json:"cve_score"`
+	AffVer   string  `json:"aff_ver"`
 	ResVer   string  `json:"res_ver"`
+	Status   string  `json:"status"`
 }
