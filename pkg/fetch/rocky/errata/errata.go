2131df84517438f59b96bd7783ee2e596ead3fea
MaineK00n <mainek00n.1229@gmail.com>
2025-11-27 00:40:33 +0900

    chore(fetch/rocky/errata): skip 500 advisory

--- pkg/fetch/rocky/errata/errata.go
+++ pkg/fetch/rocky/errata/errata.go
@@ -1,6 +1,7 @@
 package errata
 
 import (
+	"context"
 	"encoding/json/v2"
 	"fmt"
 	"io"
@@ -9,13 +10,15 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/hashicorp/go-retryablehttp"
 	"github.com/pkg/errors"
+	"github.com/schollz/progressbar/v3"
 
 	"github.com/MaineK00n/vuls-data-update/pkg/fetch/util"
 	utilhttp "github.com/MaineK00n/vuls-data-update/pkg/fetch/util/http"
 )
 
-const dataURL = "https://errata.build.resf.org/api/v2/advisories?page=%d&limit=25"
+const dataURL = "https://errata.build.resf.org/api/v2/advisories?page=%d&limit=1"
 
 type options struct {
 	dataURL string
@@ -57,6 +60,8 @@ func WithRetry(retry int) Option {
 	return retryOption(retry)
 }
 
+var errNoMoreAdvisory = errors.New("no more advisory")
+
 func Fetch(opts ...Option) error {
 	options := &options{
 		dataURL: dataURL,
@@ -72,50 +77,70 @@ func Fetch(opts ...Option) error {
 		return errors.Wrapf(err, "remove %s", options.dir)
 	}
 
-	log.Printf("[INFO] Fetch Rocky Linux")
+	log.Printf("[INFO] Fetch Rocky Linux Errata")
 
+	client := utilhttp.NewClient(utilhttp.WithClientRetryMax(options.retry), utilhttp.WithClientCheckRetry(checkRetry))
+	bar := progressbar.Default(-1, "Paging Rocky Linux Errata")
 	for i := 0; ; i++ {
-		as, err := func() ([]Advisory, error) {
-			resp, err := utilhttp.NewClient(utilhttp.WithClientRetryMax(options.retry)).Get(fmt.Sprintf(options.dataURL, i))
+		if err := func() error {
+			resp, err := client.Get(fmt.Sprintf(options.dataURL, i))
 			if err != nil {
-				return nil, errors.Wrap(err, "fetch advisory")
+				return errors.Wrap(err, "fetch advisory")
 			}
 			defer resp.Body.Close()
 
-			if resp.StatusCode != http.StatusOK {
+			switch resp.StatusCode {
+			case http.StatusOK:
+				var a advisories
+				if err := json.UnmarshalRead(resp.Body, &a); err != nil {
+					return errors.Wrap(err, "decode json")
+				}
+
+				if len(a.Advisories) == 0 {
+					return errNoMoreAdvisory
+				}
+
+				for _, a := range a.Advisories {
+					splitted, err := util.Split(a.Name, "-", ":")
+					if err != nil {
+						return errors.Wrapf(err, "unexpected ID format. expected: %q, actual: %q", "RLSA-yyyy:\\d{4}", a.Name)
+					}
+					if _, err := time.Parse("2006", splitted[1]); err != nil {
+						return errors.Wrapf(err, "unexpected ID format. expected: %q, actual: %q", "RLSA-yyyy:\\d{4}", a.Name)
+					}
+
+					if err := util.Write(filepath.Join(options.dir, splitted[0], splitted[1], fmt.Sprintf("%s.json", a.Name)), a); err != nil {
+						return errors.Wrapf(err, "write %s", filepath.Join(options.dir, splitted[0], splitted[1], fmt.Sprintf("%s.json", a.Name)))
+					}
+				}
+
+				return nil
+			case http.StatusInternalServerError:
+				// https://github.com/resf/distro-tools/issues/62
 				_, _ = io.Copy(io.Discard, resp.Body)
-				return nil, errors.Errorf("error response with status code %d", resp.StatusCode)
+				return nil
+			default:
+				_, _ = io.Copy(io.Discard, resp.Body)
+				return errors.Errorf("error response with status code %d", resp.StatusCode)
 			}
-
-			var a advisories
-			if err := json.UnmarshalRead(resp.Body, &a); err != nil {
-				return nil, errors.Wrap(err, "decode json")
+		}(); err != nil {
+			if errors.Is(err, errNoMoreAdvisory) {
+				break
 			}
-
-			return a.Advisories, nil
-		}()
-		if err != nil {
 			return errors.Wrap(err, "fetch")
 		}
-
-		for _, a := range as {
-			splitted, err := util.Split(a.Name, "-", ":")
-			if err != nil {
-				return errors.Wrapf(err, "unexpected ID format. expected: %q, actual: %q", "RLSA-yyyy:\\d{4}", a.Name)
-			}
-			if _, err := time.Parse("2006", splitted[1]); err != nil {
-				return errors.Wrapf(err, "unexpected ID format. expected: %q, actual: %q", "RLSA-yyyy:\\d{4}", a.Name)
-			}
-
-			if err := util.Write(filepath.Join(options.dir, splitted[0], splitted[1], fmt.Sprintf("%s.json", a.Name)), a); err != nil {
-				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, splitted[0], splitted[1], fmt.Sprintf("%s.json", a.Name)))
-			}
-		}
-
-		if len(as) == 0 {
-			break
-		}
+		_ = bar.Add(1)
 	}
+	_ = bar.Close()
 
 	return nil
+}
+
+func checkRetry(ctx context.Context, resp *http.Response, err error) (bool, error) {
+	switch resp.StatusCode {
+	case http.StatusInternalServerError:
+		return false, nil
+	default:
+		return retryablehttp.ErrorPropagatedRetryPolicy(ctx, resp, err)
+	}
 }
