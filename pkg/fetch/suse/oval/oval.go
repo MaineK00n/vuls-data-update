31aec82b0e3bf4f092d5a67a7a325d04ea432de1
MaineK00n <mainek00n.1229@gmail.com>
2026-01-24 06:58:35 +0900

    refactor(fetch/suse/oval): walkIndexOf

--- pkg/fetch/suse/oval/oval.go
+++ pkg/fetch/suse/oval/oval.go
@@ -6,10 +6,11 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"maps"
 	"net/http"
 	"net/url"
-	"path"
 	"path/filepath"
+	"slices"
 	"strings"
 	"time"
 
@@ -107,34 +108,28 @@ func Fetch(opts ...Option) error {
 		return errors.Wrap(err, "walk index of")
 	}
 
-	us := make([]string, 0, len(ovals))
-	fnByURL := make(map[string]FileName, len(ovals))
+	m := make(map[string]ovalfile, len(ovals))
 	for _, fn := range ovals {
-		fullURL, err := url.JoinPath(options.baseURL, fn.Raw)
+		u, err := url.JoinPath(options.baseURL, fn.raw)
 		if err != nil {
 			return errors.Wrap(err, "join url path")
 		}
-
-		us = append(us, fullURL)
-		fnByURL[fullURL] = fn
+		m[u] = fn
 	}
 
-	if err := utilhttp.NewClient(utilhttp.WithClientRetryMax(options.retry)).PipelineGet(us, options.concurrency, options.wait, false, func(resp *http.Response) error {
+	if err := utilhttp.NewClient(utilhttp.WithClientRetryMax(options.retry)).PipelineGet(slices.Collect(maps.Keys(m)), options.concurrency, options.wait, false, func(resp *http.Response) error {
 		defer resp.Body.Close()
 
 		if resp.StatusCode != http.StatusOK {
 			_, _ = io.Copy(io.Discard, resp.Body)
-			return errors.Errorf("unexpected error response. expected: %d, actual: %d", http.StatusOK, resp.StatusCode)
+			return errors.Errorf("error response with status code %d", resp.StatusCode)
 		}
 
-		fn, ok := fnByURL[resp.Request.URL.String()]
+		fn, ok := m[resp.Request.URL.String()]
 		if !ok {
 			_, _ = io.Copy(io.Discard, resp.Body)
 			return errors.Errorf("unexpected response url: %s", resp.Request.URL.String())
 		}
-		ovaltype := fn.OvalType()
-		osname := fn.OS
-		version := fn.Version
 
 		r, err := gzip.NewReader(resp.Body)
 		if err != nil {
@@ -148,41 +143,41 @@ func Fetch(opts ...Option) error {
 		}
 
 		for _, def := range root.Definitions.Definition {
-			if err := util.Write(filepath.Join(options.dir, osname, version, ovaltype, "definitions", fmt.Sprintf("%s.json", def.ID)), def); err != nil {
-				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, osname, version, ovaltype, "definitions", fmt.Sprintf("%s.json", def.ID)))
+			if err := util.Write(filepath.Join(options.dir, fn.os, fn.version, fn.ovalType(), "definitions", fmt.Sprintf("%s.json", def.ID)), def); err != nil {
+				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, fn.os, fn.version, fn.ovalType(), "definitions", fmt.Sprintf("%s.json", def.ID)))
 			}
 		}
 
 		for _, test := range root.Tests.RpminfoTest {
-			if err := util.Write(filepath.Join(options.dir, osname, version, ovaltype, "tests", "rpminfo_test", fmt.Sprintf("%s.json", test.ID)), test); err != nil {
-				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, osname, version, ovaltype, "tests", "rpminfo_test", fmt.Sprintf("%s.json", test.ID)))
+			if err := util.Write(filepath.Join(options.dir, fn.os, fn.version, fn.ovalType(), "tests", "rpminfo_test", fmt.Sprintf("%s.json", test.ID)), test); err != nil {
+				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, fn.os, fn.version, fn.ovalType(), "tests", "rpminfo_test", fmt.Sprintf("%s.json", test.ID)))
 			}
 		}
 		for _, test := range root.Tests.UnameTest {
-			if err := util.Write(filepath.Join(options.dir, osname, version, ovaltype, "tests", "uname_test", fmt.Sprintf("%s.json", test.ID)), test); err != nil {
-				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, osname, version, ovaltype, "tests", "uname_test", fmt.Sprintf("%s.json", test.ID)))
+			if err := util.Write(filepath.Join(options.dir, fn.os, fn.version, fn.ovalType(), "tests", "uname_test", fmt.Sprintf("%s.json", test.ID)), test); err != nil {
+				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, fn.os, fn.version, fn.ovalType(), "tests", "uname_test", fmt.Sprintf("%s.json", test.ID)))
 			}
 		}
 
 		for _, object := range root.Objects.RpminfoObject {
-			if err := util.Write(filepath.Join(options.dir, osname, version, ovaltype, "objects", "rpminfo_object", fmt.Sprintf("%s.json", object.ID)), object); err != nil {
-				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, osname, version, ovaltype, "objects", "rpminfo_object", fmt.Sprintf("%s.json", object.ID)))
+			if err := util.Write(filepath.Join(options.dir, fn.os, fn.version, fn.ovalType(), "objects", "rpminfo_object", fmt.Sprintf("%s.json", object.ID)), object); err != nil {
+				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, fn.os, fn.version, fn.ovalType(), "objects", "rpminfo_object", fmt.Sprintf("%s.json", object.ID)))
 			}
 		}
 		if root.Objects.UnameObject.ID != "" {
-			if err := util.Write(filepath.Join(options.dir, osname, version, ovaltype, "objects", "uname_object", fmt.Sprintf("%s.json", root.Objects.UnameObject.ID)), root.Objects.UnameObject); err != nil {
-				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, osname, version, ovaltype, "objects", "uname_object", fmt.Sprintf("%s.json", root.Objects.UnameObject.ID)))
+			if err := util.Write(filepath.Join(options.dir, fn.os, fn.version, fn.ovalType(), "objects", "uname_object", fmt.Sprintf("%s.json", root.Objects.UnameObject.ID)), root.Objects.UnameObject); err != nil {
+				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, fn.os, fn.version, fn.ovalType(), "objects", "uname_object", fmt.Sprintf("%s.json", root.Objects.UnameObject.ID)))
 			}
 		}
 
 		for _, state := range root.States.RpminfoState {
-			if err := util.Write(filepath.Join(options.dir, osname, version, ovaltype, "states", "rpminfo_state", fmt.Sprintf("%s.json", state.ID)), state); err != nil {
-				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, osname, version, ovaltype, "states", "rpminfo_state", fmt.Sprintf("%s.json", state.ID)))
+			if err := util.Write(filepath.Join(options.dir, fn.os, fn.version, fn.ovalType(), "states", "rpminfo_state", fmt.Sprintf("%s.json", state.ID)), state); err != nil {
+				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, fn.os, fn.version, fn.ovalType(), "states", "rpminfo_state", fmt.Sprintf("%s.json", state.ID)))
 			}
 		}
 		for _, state := range root.States.UnameState {
-			if err := util.Write(filepath.Join(options.dir, osname, version, ovaltype, "states", "uname_state", fmt.Sprintf("%s.json", state.ID)), state); err != nil {
-				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, osname, version, ovaltype, "states", "uname_state", fmt.Sprintf("%s.json", state.ID)))
+			if err := util.Write(filepath.Join(options.dir, fn.os, fn.version, fn.ovalType(), "states", "uname_state", fmt.Sprintf("%s.json", state.ID)), state); err != nil {
+				return errors.Wrapf(err, "write %s", filepath.Join(options.dir, fn.os, fn.version, fn.ovalType(), "states", "uname_state", fmt.Sprintf("%s.json", state.ID)))
 			}
 		}
 
@@ -194,7 +189,29 @@ func Fetch(opts ...Option) error {
 	return nil
 }
 
-func (opts options) walkIndexOf() ([]FileName, error) {
+type variant string
+
+const (
+	variantNone     variant = ""
+	variantAffected variant = "affected"
+	variantPatch    variant = "patch"
+)
+
+type ovalfile struct {
+	raw     string
+	os      string
+	version string
+	variant variant
+}
+
+func (f ovalfile) ovalType() string {
+	if f.variant == variantPatch {
+		return "patch"
+	}
+	return "vulnerability"
+}
+
+func (opts options) walkIndexOf() ([]ovalfile, error) {
 	resp, err := utilhttp.NewClient(utilhttp.WithClientRetryMax(opts.retry)).Get(opts.baseURL)
 	if err != nil {
 		return nil, errors.Wrap(err, "fetch index of")
@@ -211,63 +228,157 @@ func (opts options) walkIndexOf() ([]FileName, error) {
 		return nil, errors.Wrap(err, "parse as html")
 	}
 
-	grouped, err := collectOVALFiles(d)
-	if err != nil {
-		return nil, errors.Wrap(err, "collect oval files")
-	}
-
-	fns := make([]FileName, 0, len(grouped)*2)
-	for _, variants := range grouped {
-		if fn, ok := variants[VariantAffected]; ok {
-			fns = append(fns, fn)
-		} else if fn, ok := variants[VariantNone]; ok {
-			fns = append(fns, fn)
-		}
-		if fn, ok := variants[VariantPatch]; ok {
-			fns = append(fns, fn)
-		}
-	}
-	return fns, nil
-}
-
-// collectOVALFiles parses HTML and groups filenames by OS/Version and Variant.
-func collectOVALFiles(d *goquery.Document) (map[FileKey]map[Variant]FileName, error) {
-	grouped := make(map[FileKey]map[Variant]FileName)
-	var parseErr error
-	d.Find("a").EachWithBreak(func(_ int, selection *goquery.Selection) bool {
+	m := make(map[string][]ovalfile)
+	for _, selection := range d.Find("a").EachIter() {
 		href, ok := selection.Attr("href")
 		if !ok {
-			return true
-		}
-		if strings.HasSuffix(href, "/") || href == "../" {
-			return true
-		}
-		if !strings.HasSuffix(href, ".xml.gz") {
-			return true
+			continue
 		}
 
-		fn, err := ParseFileName(path.Base(href))
-		if err != nil {
-			parseErr = errors.Wrapf(err, "parse filename: %s", href)
-			return false
-		}
+		if f := func() *ovalfile {
+			stem, ok := strings.CutSuffix(href, ".xml.gz")
+			if !ok {
+				return nil
+			}
 
-		if fn == nil {
-			return true
-		}
-		if !fn.ShouldInclude() {
-			return true
-		}
+			variant := variantNone
+			switch {
+			case strings.HasSuffix(stem, "-affected"):
+				variant = variantAffected
+				stem = strings.TrimSuffix(stem, "-affected")
+			case strings.HasSuffix(stem, "-patch"):
+				variant = variantPatch
+				stem = strings.TrimSuffix(stem, "-patch")
+			}
 
-		key := fn.Key()
-		if grouped[key] == nil {
-			grouped[key] = make(map[Variant]FileName)
+			switch {
+			case strings.HasPrefix(stem, "suse.linux.enterprise.desktop"):
+				switch v := strings.TrimPrefix(stem, "suse.linux.enterprise.desktop."); v {
+				case "10":
+					return &ovalfile{
+						raw:     href,
+						os:      "suse.linux.enterprise.desktop",
+						version: v,
+						variant: variant,
+					}
+				default:
+					// 11 and later, use suse.linux.enterprise.<version>
+					return nil
+				}
+			case strings.HasPrefix(stem, "suse.linux.enterprise.server"):
+				switch v := strings.TrimPrefix(stem, "suse.linux.enterprise.server."); v {
+				case "9", "10":
+					return &ovalfile{
+						raw:     href,
+						os:      "suse.linux.enterprise.server",
+						version: v,
+						variant: variant,
+					}
+				default:
+					// 11 and later, use suse.linux.enterprise.<version>
+					return nil
+				}
+			case strings.HasPrefix(stem, "suse.linux.enterprise.micro"):
+				switch v := strings.TrimPrefix(stem, "suse.linux.enterprise.micro."); v {
+				case "5": // ignore minor version OVALs, as minor versions are aggregated within major version OVAL
+					return &ovalfile{
+						raw:     href,
+						os:      "suse.linux.enterprise.micro",
+						version: v,
+						variant: variant,
+					}
+				default:
+					// 6 and later use suse.linux.micro.<version>
+					return nil
+				}
+			case strings.HasPrefix(stem, "suse.linux.micro"):
+				v := strings.TrimPrefix(stem, "suse.linux.micro.")
+
+				// ignore minor version OVALs, as minor versions are aggregated within major version OVAL
+				if strings.Contains(v, ".") {
+					return nil
+				}
+				return &ovalfile{
+					raw:     href,
+					os:      "suse.linux.micro",
+					version: v,
+					variant: variant,
+				}
+			case strings.HasPrefix(stem, "suse.linux.enterprise"):
+				v := strings.TrimPrefix(stem, "suse.linux.enterprise.")
+
+				// ignore minor version OVALs, as minor versions are aggregated within major version OVAL
+				if strings.Contains(v, "-sp") || strings.Contains(v, ".") {
+					return nil
+				}
+				return &ovalfile{
+					raw:     href,
+					os:      "suse.linux.enterprise",
+					version: v,
+					variant: variant,
+				}
+			case strings.HasPrefix(stem, "opensuse.leap.micro"):
+				return &ovalfile{
+					raw:     href,
+					os:      "opensuse.leap.micro",
+					version: strings.TrimPrefix(stem, "opensuse.leap.micro."),
+					variant: variant,
+				}
+			case strings.HasPrefix(stem, "opensuse.leap"):
+				return &ovalfile{
+					raw:     href,
+					os:      "opensuse.leap",
+					version: strings.TrimPrefix(stem, "opensuse.leap."),
+					variant: variant,
+				}
+			case strings.HasPrefix(stem, "opensuse.tumbleweed"):
+				return &ovalfile{
+					raw:     href,
+					os:      "opensuse.tumbleweed",
+					version: "",
+					variant: variant,
+				}
+			case strings.HasPrefix(stem, "opensuse"):
+				return &ovalfile{
+					raw:     href,
+					os:      "opensuse",
+					version: strings.TrimPrefix(stem, "opensuse."),
+					variant: variant,
+				}
+			default:
+				return nil
+			}
+		}(); f != nil {
+			m[fmt.Sprintf("%s:%s", f.os, f.version)] = append(m[fmt.Sprintf("%s:%s", f.os, f.version)], *f)
 		}
-		grouped[key][fn.Variant] = *fn
-		return true
-	})
-	if parseErr != nil {
-		return nil, parseErr
 	}
-	return grouped, nil
+
+	fns := make([]ovalfile, 0, len(m)*2)
+	for e, os := range m {
+		var vo *ovalfile
+		for _, o := range os {
+			switch o.variant {
+			case variantNone:
+				if vo == nil {
+					vo = &o
+				}
+			case variantAffected:
+				vo = &o
+			case variantPatch:
+				fns = append(fns, o)
+			default:
+				return nil, errors.Errorf("unexpected variant. expected: %q, actual: %q", []variant{variantNone, variantAffected, variantPatch}, o.variant)
+			}
+		}
+		if vo == nil {
+			// sanity check, but opensuse.leap:13.2 has -patch variant only
+			switch e {
+			case "opensuse.leap:13.2":
+			default:
+				return nil, errors.Errorf("no vulnerability oval found for %s", e)
+			}
+		}
+		fns = append(fns, *vo)
+	}
+	return fns, nil
 }
