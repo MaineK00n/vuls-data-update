fefe55e4e424707af747953cc6e984cd7d61bd4c
Shunichi Shinohara <shino.shun@gmail.com>
2026-03-18 17:41:46 +0900

    fix!(criterion/cpe): correct CPE accept semantics

--- pkg/extract/types/data/detection/condition/criteria/criterion/versioncriterion/package/cpe/cpe.go
+++ pkg/extract/types/data/detection/condition/criteria/criterion/versioncriterion/package/cpe/cpe.go
@@ -11,15 +11,15 @@ type CPE string
 type Query string
 
 func (p CPE) Accept(query Query) (bool, error) {
-	wfn1, err := naming.UnbindFS(string(query))
+	queryWFN, err := naming.UnbindFS(string(query))
 	if err != nil {
 		return false, errors.Wrapf(err, "unbind %q to WFN", string(query))
 	}
 
-	wfn2, err := naming.UnbindFS(string(p))
+	patternWFN, err := naming.UnbindFS(string(p))
 	if err != nil {
 		return false, errors.Wrapf(err, "unbind %q to WFN", string(p))
 	}
 
-	return matching.IsSubset(wfn1, wfn2), nil
+	return !matching.IsDisjoint(queryWFN, patternWFN), nil
 }
